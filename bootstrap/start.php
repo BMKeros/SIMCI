b66163ca83ee3949f106acff8f7032413b71af94
Israel Lugo <hostelixisrael@gmail.com>
2015-11-30 09:50:03 -0430

    [MOD] modificaciones en  la variables de entorno

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,7 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array($_SERVER['USER_ENVIROMENT']),
+	'local' => array(explode($_SERVER['SERVER_ADMIN'],'@')[0]),
 
 ));
 
