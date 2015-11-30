c3272ad1d3943c7943e35d1ca4dbd402719ac71b
daniels <danztdw@gmail.com>
2015-11-30 10:38:42 -0430

    [MOD]nombres de las vistas y [ADD] vista nueva

--- app/views/inicio/inicio.blade.php
+++ app/views/inicio/inicio.blade.php
@@ -38,37 +38,37 @@
       	<div class="three wide column">
         	<img src="/semantic/themes/default/assets/images/atom.gif" width="70%" height="50%"> <!--Icono de Prueba-->
       	</div>
-    <div class="formulario" id="inicio_sesion">
-    	<div class="ten wide column">
-    		<div class="ui two column middle aligned very relaxed stackable grid">
-			  	<div class="column">
-			  		<div class="ui orange message">
-			  			<div id="ident"> <p> Inicio Sesion</p> </div>
-			  		</div>
-   			    	<div class="ui form">
-			      		<div class="field">
-			        		<label>Usuario</label>
-			        		<div class="ui left icon input">
-			          			<input type="text" placeholder="Usuario">
-			          			<i class="user icon"></i>
-			        		</div>
-			      		</div>
-			      	
-			      	<div class="field">
-			        	<label>Password</label>
-			        	<div class="ui left icon input">
-			          		<input type="password" placeholder="Password">
-			          		<i class="lock icon"></i>
-			        	</div>
-			      	</div>
-			      	
-			      	<div class="ui blue submit button">Login</div>
-			    	
-			    	</div>
-			 	</div>
+	    <div class="formulario" id="inicio_sesion">
+	    	<div class="ten wide column">
+	    		<div class="ui two column middle aligned very relaxed stackable grid">
+				  	<div class="column">
+				  		<div class="ui blue message">
+				  			<div id="ident"> <p> Inicio Sesion</p> </div>
+				  		</div>
+	   			    	<div class="ui form">
+				      		<div class="field">
+				        		<label>Usuario</label>
+				        		<div class="ui left icon input">
+				          			<input type="text" placeholder="Usuario">
+				          			<i class="user icon"></i>
+				        		</div>
+				      		</div>
+				      	
+				      	<div class="field">
+				        	<label>Password</label>
+				        	<div class="ui left icon input">
+				          		<input type="password" placeholder="Password">
+				          		<i class="lock icon"></i>
+				        	</div>
+				      	</div>
+				      	
+				      	<div class="ui blue submit button">Login</div>
+				    	
+				    	</div>
+				 	</div>
+				</div>
 			</div>
-		</div>
-    </div>
+	    </div>
     
     </div>
 </div>
