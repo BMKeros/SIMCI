fd33cb708911604ab996a9bfe036924efe95c85e
daniels <danztdw@gmail.com>
2015-12-01 13:36:32 -0430

    [ADD]Botones de notificaciones y ajustes mas Boton de menu-contenido

--- app/views/layouts/plantilla_usuarios_base.blade.php
+++ app/views/layouts/plantilla_usuarios_base.blade.php
@@ -6,11 +6,9 @@
 	@section('Usuario')
 		<title>Usuario-SIMCI</title>
 	@show
-
-	<script src="js/jquery.min.js"></script>
 	
   <link rel="stylesheet" href="/semantic/semantic.min.css">
-	<link rel="stylesheet" type="text/css" href="/semantic/formulario_inicio_sesion.css">
+	<link rel="stylesheet" type="text/css" href="/css/formulario_inicio_sesion.css">
 	@section ('css-usuario')
 	@show
 </head>
@@ -19,80 +17,137 @@
 
 @section('barra-usuario')
 <header><!--Barra de navegacion usuario-profesor-->
-	<div class="barra-nav" id="barra">
-		<div class="ui attached stackable menu">
-		  	<div class="ui container">
-		    	<a class="item"><i class="home icon"></i> SIMCI</a>
-		    	<div class="right item">
-		    	<div class="ui input"><input type="text" placeholder="Search..."></div>
-		    	</div>
-		  	</div>
-		</div>
-	</div>
+  <div class="ui attached stackable menu">
+    <a class="item"><i class="home icon"></i> SIMCI</a>
+
+    <div class="right item "><!--Dropdown para notificaciones-->
+      <div class="ui floating labeled icon dropdown button" style="padding-left=20px; left:420px">
+        <i class="alarm icon"></i>
+        <span class="text">Notificaciones</span>
+        <div class="menu">
+          <div class="header">
+            Buscar
+          </div>
+          <div class="ui left icon input">
+            <i class="search icon"></i>
+            <input type="text" name="search" placeholder="Buscar . . .">
+          </div>
+          <div class="header">
+            <i class="flag icon"></i>
+            Todas las notificaciones
+          </div>
+          <div class="item">
+            <div class="ui red empty circular label"></div>
+            Solicitudes
+          </div>
+          <div class="item">
+            <div class="ui blue empty circular label"></div>
+            Reportes
+          </div>
+          <div class="item">
+            <div class="ui black empty circular label"></div>
+            Actualizaciones
+          </div>
+        </div>
+      </div>
+    </div><!--Fin del Dropdown de Notificaciones-->
+
+    	<div class="right item" style="padding-right: 80px; "><!--Css para ubicar dropdown-->
+        <div class="ui floating labeled icon dropdown button"><!--Dropdwn para ajustes-->
+          <i class="user icon"></i>
+          <span class="text">Usuario</span>
+          <div class="menu">
+            <div class="header">
+              <i class="tags icon"></i>
+              Opciones
+            </div>
+            <div class="divider"></div>
+            <div class="item">
+              <i class="plus icon"></i>
+              Empty
+            </div>
+            <div class="item">
+              <i class="configure icon"></i>
+              Configuracion
+            </div>
+            <div class="item">
+              <i class="sign out icon"></i>
+              Salir
+            </div>
+          </div>
+        </div><!--Fin del Dropdown Ajustes-->
+    	</div>
+  </div>  
 </header><!--Fin barra de navegacion principal de usuario-profesor-->
 @show
 
 @section('menu-usuario')
-<div class="ui grid" class="desplegable">
+<div class="ui grid" id="desplegable">
   <!--Menu Desplegable de Usuario-Profesor-->
-  <div class="ui left vertical inverted labeled icon sidebar menu" id="usuario">
-  	<a class="item">
-    	<i class="home icon"></i>
-    	Home
-  	</a>
-  	
-  	<a class="item">
-    	<i class="block layout icon"></i>
-    	Topics
-  	</a>
-  	
-  	<a class="item">
-    	<i class="smile icon"></i>
-    	Friends
-  	</a>
-  	
-  	<a class="item">
-    	<i class="calendar icon"></i>
-    	History
-  	</a>
-  	
-  	<a class="item">
-    	<i class="mail icon"></i>
-    	Messages
-  	</a>
-  	
-  	<a class="item">
-    	<i class="chat icon"></i>
-    	Discussions
-  	</a>
-  	
-  	<a class="item">
-    	<i class="trophy icon"></i>
-    	Achievements
-  	</a>
-  	
-  	<a class="item">
-    	<i class="shop icon"></i>
-    	Store
-  	</a>
-  	
-  	<a class="item">
-    	<i class="settings icon"></i>
-    	Settings
-  	</a>
-  </div>
+  <div class="eight column row">
+    <div class="ui left vertical inverted labeled icon sidebar menu" id="usuario">
+    	<a class="item">
+      	<i class="home icon"></i>
+      	Home
+    	</a>
+    	
+    	<a class="item">
+      	<i class="block layout icon"></i>
+      	Topics
+    	</a>
+    	
+    	<a class="item">
+      	<i class="smile icon"></i>
+      	Friends
+    	</a>
+    	
+    	<a class="item">
+      	<i class="calendar icon"></i>
+      	History
+    	</a>
+    	
+    	<a class="item">
+      	<i class="mail icon"></i>
+      	Messages
+    	</a>
+    	
+    	<a class="item">
+      	<i class="chat icon"></i>
+      	Discussions
+    	</a>
+    	
+    	<a class="item">
+      	<i class="trophy icon"></i>
+      	Achievements
+    	</a>
+    	
+    	<a class="item">
+      	<i class="shop icon"></i>
+      	Store
+    	</a>
+    	
+    	<a class="item">
+      	<i class="settings icon"></i>
+      	Settings
+    	</a>
+    </div>
 
 
-
-  <div class="ui vertical animated button" tabindex="0" id="menu">
-    <div class="hidden content">Menu</div>
-    <div class="visible content">
-      <i class="sidebar icon"></i>
+    <div class="ui animated fade big launch button" tabindex="1" id="menu">
+      <div class="hidden content">
+        <div id="botton">
+          Menu
+        </div>
+      </div>
+      <div class="visible content">
+        <i class="sidebar icon"></i>
+      </div>
     </div>
   </div>
 </div>
 
 @show
+<script src="js/jquery.min.js"></script>
 <script src="js/usuario.js"></script>
 <script src="semantic/semantic.min.js"></script>	
 @section('js')
