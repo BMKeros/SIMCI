0a444e0718c12ced32031fe113ee4fde9dc9c323
Israel Lugo <hostelixisrael@gmail.com>
2015-11-30 23:27:25 -0430

    [FIX] reparado errores en las reglas de validacion de usuarios

--- public/js/script_formularios.js
+++ public/js/script_formularios.js
@@ -1,28 +1,28 @@
 var reglas_formulario_login = {
-	fields: {
-  		usuario: {
-    		identifier: 'usuario',
+   fields: {
+      usuario: {
+         identifier: 'usuario',
     		rules:[
-      			{
-	            	type:'empty',
-	            	prompt:'Este campo no puede quedar vacio, y debe contene entre 5 y 20 caracteres'
-	          	},
-	          	{
-	        		type:'minLength[5]'
-	        		prompt:'Elcampo usuario debe contener minimo 5 caracteres'
+            {
+               type:'empty',
+	            prompt:'Este campo no puede quedar vacio, y debe contene entre 5 y 20 caracteres'
+	         },
+	         {
+	        	   type:'minLength[5]',
+	        	   prompt:'Elcampo usuario debe contener minimo 5 caracteres'
 	        	},
-	        	{
-	        		type:'maxLength[20]'
-	        		prompt:'Elcampo usuario no debe exceder los 20 caracteres'
-	        	}
-	        ]
-  		}
+            {
+               type:'maxLength[20]',
+               prompt:'Elcampo usuario no debe exceder los 20 caracteres'
+            }
+	     ]
+  		},
   		password:{
   			identifier: 'password',
   			ruler:[
   				{
   					type:'empty',
-	            	prompt:'Este campo no puede quedar vacio, y debe contene entre 5 y 20 caracteres'
+	            prompt:'Este campo no puede quedar vacio, y debe contene entre 5 y 20 caracteres'
   				}
   			]
   		}
