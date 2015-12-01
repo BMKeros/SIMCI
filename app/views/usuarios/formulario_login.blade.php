adc3f437067fce4ebf32a4b27678063b85e14445
Israel Lugo <hostelixisrael@gmail.com>
2015-12-01 14:22:11 -0430

    [MOD]modificaciones en la vista de plantilla_usuario_base

--- app/views/usuarios/formulario_login.blade.php
+++ app/views/usuarios/formulario_login.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 	<head>
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -73,11 +73,7 @@
 
 		<script>
 		$(document).ready(function(){
-			
 			$('.ui.form').form(reglas_formulario_login);
-
-			
-			
 		});
 		</script>
 		
