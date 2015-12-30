69d60ff152d9f244e4fd720e314a55247c2d6065
Israel Lugo <hostelixisrael@gmail.com>
2015-12-30 18:36:34 -0430

    [ADD] agregado el controlador de modulos para separar cada modulo

---
+++ app/controllers/ModulosController.php
@@ -0,0 +1,16 @@
+<?php
+
+class ModulosController extends Controller {
+
+	public function getAdministracion(){
+		return "Modulo administracion";
+	}
+
+	public function getProfesores(){
+		return "Modulo administracion";
+	}
+
+	public function getEstudiantes(){
+		return "Modulo usurios";
+	}
+}
