359b6d02bc256ac48eecf96251186c292dbe7eac
Israel Lugo <hostelixisrael@gmail.com>
2015-12-30 19:32:34 -0430

    [ADD] agregado la ruta modulos

--- app/routes.php
+++ app/routes.php
@@ -15,7 +15,5 @@ Route::get('/', array('uses' => 'AutenticacionController@getLogin'));
 
 Route::controller('/usuarios', 'UsuariosController');//ruta para los usuarios
 Route::controller('/autenticacion', 'AutenticacionController');
-Route::controller('/busquedas', 'BusquedasController');//ruta para todas las busquedas del sistema
-
-
-
+Route::controller('/buscar', 'BusquedasController');//ruta para todas las busquedas del sistema
+Route::controller('/modulos', 'ModulosController');
