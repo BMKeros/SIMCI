2648d266fc51cbc5b721dd758a5dbe6f69a728b0
Israel Lugo <hostelixisrael@gmail.com>
2015-12-30 19:31:05 -0430

    [ADD] agregada la nueva funcion de redirect_pot_tipo su utilidad es para redirigir al usuario dependiendo de su tipo

--- app/helpers/helpers_db.php
+++ app/helpers/helpers_db.php
@@ -5,5 +5,14 @@ function drop_cascade($table = null){
 	if( Schema::hasTable($table) ){
 		DB::statement('DROP TABLE '.$table.' CASCADE;');
 	}
+}
 
+function redirect_por_tipo($tipo_usuario = null){
+	if($tipo_usuario == TIPO_USER_ROOT ){
+		return Redirect::action('ModulosController@getAdministracion');	
+	}
+	else{
+		Auth::logout();
+		return Redirect::to('/')->with(array('mensaje_alerta'=>"Error ha ocurrido un problema con su tipo de usuario"));		
+	}
 }
