4a6f4dead712538ce29b69c0a476d7920db224e9
daniels <danztdw@gmail.com>
2016-01-06 19:07:59 -0430

    [MOD] Repare la url y detalles en formulario de registrar-usuario

--- app/views/usuarios/registrar_usuario.blade.php
+++ app/views/usuarios/registrar_usuario.blade.php
@@ -5,7 +5,7 @@
 		    	<div class="ten wide column">
 				  	<div class="column">
 
-	   			    	<form class="ui form" id="form-inicio-sesion" action="/autenticacion/login" method="POST">
+	   			    	<form class="ui form" id="form-inicio-sesion">
 					      	<div class="field">
 				        		<div class="ui left icon input">
 				          			<input type="text" placeholder="Usuario" id="usuario" name="usuario">
@@ -15,7 +15,7 @@
 
 					      	<div class="field">
 				        		<div class="ui left icon input">
-				          			<input type="text" placeholder="Email" id="email" name="usuario">
+				          			<input type="text" placeholder="Email" id="email" name="email">
 				          			<i class="mail square icon"></i>
 				        		</div>
 			      			</div>
@@ -29,7 +29,7 @@
 
 					      	<div class="field">
 					        	<div class="ui left icon input">
-					          		<input type="password" placeholder="Confirmar Password" id="confirmar-password" name="password">
+					          		<input type="password" placeholder="Confirmar Password" id="confirmar-password" name="confi-password">
 					          		<i class="lock icon"></i>
 					        	</div>
 					      	</div>
