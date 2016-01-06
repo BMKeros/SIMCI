87a30cda3956d831bbb2d54abf679a63658c471b
daniels <danztdw@gmail.com>
2016-01-06 17:54:32 -0430

    [MOD]Modifique vista registrar usuario, para la vista correspondiente, y [ADD]una plantilla que habia desaparecido.

--- app/views/usuarios/registrar_usuario.blade.php
+++ app/views/usuarios/registrar_usuario.blade.php
@@ -1,49 +1,45 @@
-@extends('modulos.dashboard_estudiante')
+@extends('layouts.plantilla_usuarios_base')
 	@section('barra-usuarios')
 	@parent
-		<div class="ui ten column doubling stackable grid container">
+		<div class="ui two column doubling stackable grid container">
 			<div class="ui one column centered grid">
-				<div class="ten wide column">
-				<div class="column"> </div>
-					<form class="ui form" id="formulario-registro-usuario">
-			      		<div class="field">
-			        		<div class="ui left icon input">
-			          			<input type="text" placeholder="Usuario" id="usuario" name="usuario">
-			          			<i class="user icon"></i>
-			        		</div>
-			      		</div>
-			      		
-			      		<div class="field">
-			        		<div class="ui left icon input">
-			          			<input type="text" placeholder="Email" id="email" name="usuario">
-			          			<i class="mail square icon"></i>
-			        		</div>
-			      		</div>
+				<div class="column">
+					<div class="formulario" id="inicio_sesion">
+				    	<div class="ten wide column">
+						  	<div class="column">
 
-			      		<div class="field">
-							<div class="ui left icon input">
-								<input type="text" placeholder="Clave" id="clave" name="clave">
-								<i class="hide icon"></i>
-							</div>
-			      		</div>
-				      	
-				      	<div class="field">
-				        	<div class="ui left icon input">
-				          		<input type="Password" placeholder="Password" id="password" name="password">
-				          		<i class="lock icon"></i>
-				        	</div>
-				      	</div>
+			   			    	<form class="ui form" id="form-inicio-sesion" action="/autenticacion/login" method="POST">
+							      	<div class="field">
+						        		<div class="ui left icon input">
+						          			<input type="text" placeholder="Usuario" id="usuario" name="usuario">
+						          			<i class="user icon"></i>
+						        		</div>
+					      			</div>
 
-				      	<div class="field">
-				        	<div class="ui left icon input">
-				          		<input type="password" placeholder="Confirmar Password" id="confirmar-password" name="password">
-				          		<i class="lock icon"></i>
-				        	</div>
-				      	</div>
+							      	<div class="field">
+						        		<div class="ui left icon input">
+						          			<input type="text" placeholder="Email" id="email" name="usuario">
+						          			<i class="mail square icon"></i>
+						        		</div>
+					      			</div>
+					      	
+							      	<div class="field">
+							        	<div class="ui left icon input">
+							          		<input type="Password" placeholder="Password" id="password" name="password">
+							          		<i class="lock icon"></i>
+							        	</div>
+							      	</div>
 
-						<button class="short ui right floated blue submit button"  id="btn-inicio-sesion"><i class="checkmark icon"></i>Registrar</button>
-			    	</form>
-			    </div>
+							      	<div class="field">
+							        	<div class="ui left icon input">
+							          		<input type="password" placeholder="Confirmar Password" id="confirmar-password" name="password">
+							          		<i class="lock icon"></i>
+							        	</div>
+							      	</div>
+							    </form>
+						  	</div>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
