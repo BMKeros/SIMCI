0e8111af00d16aed0fa9afb25f7976f257b2533e
daniels <danztdw@gmail.com>
2016-01-06 18:57:31 -0430

    [DEL] elimine el Blade de la vista registrar usuario

--- app/views/usuarios/registrar_usuario.blade.php
+++ app/views/usuarios/registrar_usuario.blade.php
@@ -1,52 +1,42 @@
-@extends('layouts.plantilla_usuarios_base')
-	@section('barra-usuarios')
-	@parent
-		<div class="ui two column doubling stackable grid container">
-			<div class="ui one column centered grid">
-				<div class="column">
-					<div class="formulario" id="inicio_sesion">
-				    	<div class="ten wide column">
-						  	<div class="column">
+<div class="ui two column doubling stackable grid container">
+	<div class="ui one column centered grid">
+		<div class="column">
+			<div class="formulario" id="inicio_sesion">
+		    	<div class="ten wide column">
+				  	<div class="column">
 
-			   			    	<form class="ui form" id="form-inicio-sesion" action="/autenticacion/login" method="POST">
-							      	<div class="field">
-						        		<div class="ui left icon input">
-						          			<input type="text" placeholder="Usuario" id="usuario" name="usuario">
-						          			<i class="user icon"></i>
-						        		</div>
-					      			</div>
+	   			    	<form class="ui form" id="form-inicio-sesion" action="/autenticacion/login" method="POST">
+					      	<div class="field">
+				        		<div class="ui left icon input">
+				          			<input type="text" placeholder="Usuario" id="usuario" name="usuario">
+				          			<i class="user icon"></i>
+				        		</div>
+			      			</div>
 
-							      	<div class="field">
-						        		<div class="ui left icon input">
-						          			<input type="text" placeholder="Email" id="email" name="usuario">
-						          			<i class="mail square icon"></i>
-						        		</div>
-					      			</div>
-					      	
-							      	<div class="field">
-							        	<div class="ui left icon input">
-							          		<input type="Password" placeholder="Password" id="password" name="password">
-							          		<i class="lock icon"></i>
-							        	</div>
-							      	</div>
+					      	<div class="field">
+				        		<div class="ui left icon input">
+				          			<input type="text" placeholder="Email" id="email" name="usuario">
+				          			<i class="mail square icon"></i>
+				        		</div>
+			      			</div>
+			      	
+					      	<div class="field">
+					        	<div class="ui left icon input">
+					          		<input type="Password" placeholder="Password" id="password" name="password">
+					          		<i class="lock icon"></i>
+					        	</div>
+					      	</div>
 
-							      	<div class="field">
-							        	<div class="ui left icon input">
-							          		<input type="password" placeholder="Confirmar Password" id="confirmar-password" name="password">
-							          		<i class="lock icon"></i>
-							        	</div>
-							      	</div>
-							    </form>
-						  	</div>
-						</div>
-					</div>
+					      	<div class="field">
+					        	<div class="ui left icon input">
+					          		<input type="password" placeholder="Confirmar Password" id="confirmar-password" name="password">
+					          		<i class="lock icon"></i>
+					        	</div>
+					      	</div>
+					    </form>
+				  	</div>
 				</div>
 			</div>
 		</div>
-	@endsection
-	
-	<script src="js/jquery.min.js"></script>
-	<script src="js/scripts_plantilla_usuario.js"></script>
-	<script src="semantic/semantic.min.js"></script>	
-	@section('js')
-	@stop
+	</div>
+</div>
