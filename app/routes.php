7899f4694a624dda50c0ab636afcac4c63c68a42
Israel Lugo <hostelixisrael@gmail.com>
2016-01-11 23:47:53 -0430

    [MOD] agregado el prefijo api a los routes

--- app/routes.php
+++ app/routes.php
@@ -26,7 +26,13 @@ Route::get('/views/{folder}/{name_template}', function($folder,$name_template) {
   	}
 });
 
-Route::controller('/usuarios', 'UsuariosController');//ruta para los usuarios
+//Aqui van todos los controladores que se prestaran como api
+
+Route::group(array('prefix' => 'api'), function(){
+	Route::controller('/usuarios', 'UsuariosController');//ruta para los usuarios
+});
+
+
 Route::controller('/autenticacion', 'AutenticacionController');
 Route::controller('/buscar', 'BusquedasController');//ruta para todas las busquedas del sistema
 Route::controller('/modulos', 'ModulosController');
