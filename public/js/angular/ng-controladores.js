c989736119ab6c0e2120a8f7d296e3fb1de27e5c
Israel Lugo <hostelixisrael@gmail.com>
2016-01-12 02:26:53 -0430

    [MOD][ADD] modificado los controladores para adaptarlos a layout main, se agrego un objeto modulo el cual lleva ciertos atributos como nombre, icono, opciones

--- public/js/angular/ng-controladores.js
+++ public/js/angular/ng-controladores.js
@@ -9,13 +9,40 @@
 
     simci.controller('UsuariosController', ['$scope','$http','$log','$route', '$routeParams', '$location', 
       function ($scope, $http, $log , $route, $routeParams, $location){
-        
-        $scope.opciones = [
-          {nombre:"Crear Usuarios"},
-          {nombre:"Ver Usuarios"},
-          {nombre:"Eliminar Usuarios"},
-          {nombre:"Modificar Usuarios"},
-          {nombre:"Actualizar Usuarios"}
+        $scope.modulo = {};
+
+        $scope.modulo.nombre = "Usuarios";
+        $scope.modulo.icono = {
+          tipo: "users",
+          color: "blue"
+        };
+
+        $scope.modulo.opciones = [
+          {
+            nombre:"crear usuarios",
+            descripcion: "Opcion para crear nuevos usuarios en el sistema",
+            url: "#/usuarios/crear"
+          },
+          {
+            nombre:"ver usuarios",
+            descripcion: "Opcion para Ver, Actualizar, Eliminar los usuarios registrados en el sistema",
+            url: "#/usuarios/ver/todos"
+          },
+          {
+            nombre:"crear permisos",
+            descripcion: "Opcion se podran crear nuevos permisos de usuarios para el sistema",
+            url: "#/usuarios/eliminar"
+          },
+          {
+            nombre:"ver permisos",
+            descripcion: "Opcion para Ver, Actualizar, Eliminar los permisos registrados en el sistema",
+            url: "#/usuarios/modificar"
+          },
+          {
+            nombre:"crear tipos de usuario",
+            descripcion: "Opcion para Ver, Actualizar, Eliminar los permisos registrados en el sistema",
+            url: "#/usuarios/modificar"
+          },
         ];
         
         $log.info($routeParams);
@@ -35,6 +62,34 @@
             console.log(data_error);
           });
         }
+      }]
+    );
+
+
+    simci.controller('InventarioController', ['$scope','$http','$log','$route', '$routeParams', '$location', 
+      function ($scope, $http, $log , $route, $routeParams, $location){
+        
+        $scope.modulo = {};
+
+        $scope.modulo.nombre = "Inventario";
+        $scope.modulo.icono = {
+          tipo: "archive",
+          color: "green"
+        };
+        
+        $scope.modulo.opciones = [
+          {
+            nombre:"crear almacen",
+            descripcion: "Opcion para crear nuevos usuarios en el sistema",
+            url: "#/usuarios/crear"
+          },
+        ];
+        
+        $log.info($routeParams);
+        $log.info($location);
+
+
+        
       }]
     );
     
