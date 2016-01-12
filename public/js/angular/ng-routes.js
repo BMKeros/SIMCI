1629e943968e809af60f8056de607bba82e3eba5
Israel Lugo <hostelixisrael@gmail.com>
2016-01-12 02:29:14 -0430

    [MOD] modificado el ng-route agregadas las nuevas rutas de inventario y las de usuarios

--- public/js/angular/ng-routes.js
+++ public/js/angular/ng-routes.js
@@ -3,12 +3,36 @@
   if( typeof GlobalApp !== 'undefined'){
  
     GlobalApp.config(['$routeProvider','$locationProvider',function($routeProvider,$locationProvider) {
-      //Esto es solo de prueba asi es que debe llevar el orden
+      
+      //Rutas para usuarios
       $routeProvider.
         when('/usuarios', {
+          templateUrl: '/views/layouts/layout_main_modulos',
+          controller: 'UsuariosController'
+        }).
+        when('/usuarios/crear', {
+          templateUrl: '/views/usuarios/registrar_usuario',
+          controller: 'UsuariosController'
+        }).
+        when('/usuarios/ver/:param', {
           templateUrl: '/views/usuarios/principal_usuarios',
           controller: 'UsuariosController'
         }).
+        when('/usuarios/eliminar/:idUsuario', {
+          templateUrl: '/views/usuarios/principal_usuarios',
+          controller: 'UsuariosController'
+        }).
+        when('/usuarios/modificar/:idUsuario', {
+          templateUrl: '/views/usuarios/principal_usuarios',
+          controller: 'UsuariosController'
+        }).
+
+        //Rutas para inventario
+        when('/inventario', {
+          templateUrl: '/views/layouts/layout_main_modulos',
+          controller: 'InventarioController'
+        }).
+
         otherwise({
           redirectTo: function(){
 
