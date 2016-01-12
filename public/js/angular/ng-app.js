60fe8a4a0b7b1a0672fd0b9619cf3adf89aa3611
Israel Lugo <hostelixisrael@gmail.com>
2016-01-12 01:29:38 -0430

    [MOD] agregado filtro capitalize a la app simci

--- public/js/angular/ng-app.js
+++ public/js/angular/ng-app.js
@@ -4,6 +4,12 @@
           $interpolateProvider.endSymbol('%>');
   });
 
+  simci.filter('capitalize', function() {
+    return function(input, all) {
+      var reg = (all) ? /([^\W_]+[^\s-]*) */g : /([^\W_]+[^\s-]*)/;
+      return (!!input) ? input.replace(reg, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();}) : '';
+    }
+  });
    
   //Seteamos de manera global la app simci
   window.simci = simci;
