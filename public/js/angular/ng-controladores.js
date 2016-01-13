e9ad9efc19e80b3163684f107145e2b1de50d1f0
Israel Lugo <hostelixisrael@gmail.com>
2016-01-13 00:31:37 -0430

    [MOD] modificada algunas de las opciones de los menus, cambiado iconos y otras cosas

--- public/js/angular/ng-controladores.js
+++ public/js/angular/ng-controladores.js
@@ -82,10 +82,70 @@
         
         $scope.modulo.opciones = [
           {
-            nombre:"crear almacen",
-            descripcion: "Opcion para crear nuevos usuarios en el sistema",
-            url: "#/usuarios/crear"
+            nombre:"registrar elemento",
+            descripcion: "Esta opcion le permitira crear nuevos elementos en el inventario",
+            url: "#/"
           },
+          {
+            nombre:"ver inventario",
+            descripcion: "Esta opcion le permitira ver los elementos en el inventario, a su vez tambien podra modificar o eliminar dichos objetos",
+            url: "#/"
+          },
+          {
+            nombre:"crear almacen",
+            descripcion: "Opcion para crear nuevos almacenes",
+            url: "#/"
+          },
+          {
+            nombre:"crear estante",
+            descripcion: "Opcion para crear nuevos estantes",
+            url: "#/"
+          },
+          {
+            nombre:"crear agrupacion",
+            descripcion: "Esta opcion le permitira crear las agrupaciones por las cuales se ordenaran los elementos",
+            url: "#/"
+          },
+          {
+            nombre:"crear subagrupacion",
+            descripcion: "Opcion para crear alguna caracteristica por la cual tambien ordenar elementos",
+            url: "#/"
+          },
+          
+        ];
+        
+        $log.info($routeParams);
+        $log.info($location);
+
+
+        
+      }]
+    );
+
+
+    simci.controller('CatalogoController', ['$scope','$http','$log','$route', '$routeParams', '$location', 
+      function ($scope, $http, $log , $route, $routeParams, $location){
+        
+        $scope.modulo = {};
+
+        $scope.modulo.nombre = "Catalogo";
+        $scope.modulo.icono = {
+          tipo: "book",
+          color: "orange"
+        };
+        
+        $scope.modulo.opciones = [
+          {
+            nombre:"registrar objeto",
+            descripcion: "Esta opcion le permitira crear nuevos objetos en el inventario",
+            url: "#/"
+          },
+          {
+            nombre:"ver inventario",
+            descripcion: "Esta opcion le permitira ver los objetos en el inventario, a su vez tambien podra modificar o eliminar dichos objetos",
+            url: "#/"
+          },
+          
         ];
         
         $log.info($routeParams);
