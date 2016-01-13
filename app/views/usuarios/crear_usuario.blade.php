cc97a4fb0b695e0e5af78c662933c38c02e1bfa8
daniels <danztdw@gmail.com>
2016-01-13 12:03:29 -0430

    [ADD]Vista del CRUD de usuario, Actualizar y Crear Usuario

---
+++ app/views/usuarios/crear_usuario.blade.php
@@ -0,0 +1,38 @@
+<div class="ui two column doubling stackable grid container">
+	<div class="ui one column centered grid">
+		<div class="column">
+	    	<div class="ten wide column">
+			  	<div class="column">
+					<div class="ui form">
+						<form class="ui form">
+						  	<h4 class="ui dividing header">Crear Usuario</h4>
+							  	<div class="field">
+								    <div class="fields">
+								      	<div class="six wide field">
+								        	<input type="text" name="usuario" placeholder="Usuario">
+								      	</div>
+
+								      	<div class="nine wide field">
+											<input type="text" name="Email" placeholder="Direccion Email">
+										</div>
+								    </div>
+						  		</div>
+
+								<div class="field">
+								    <div class="two wide fields">
+										<div class="field">	
+								        	<input type="password" name="password" placeholder="Password">
+								        </div>
+
+								      	<div class="field">
+								        	<input type="password" name="password" placeholder="Confirmar Password">
+								      	</div>
+								    </div>
+								</div>
+						</form>
+			  		</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
