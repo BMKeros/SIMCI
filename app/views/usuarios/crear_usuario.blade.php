cc407533358ef78b90049ce0a2e3ccba654e37c2
daniels <danztdw@gmail.com>
2016-01-13 14:51:27 -0430

    [MOD]Vista Crear usuario agregando dropdown faltantes. [ADD] tabla de mostrar usuarios, esta incompleta,. Solo para muestra de tabla

--- app/views/usuarios/crear_usuario.blade.php
+++ app/views/usuarios/crear_usuario.blade.php
@@ -1,3 +1,4 @@
+@extends('layouts.plantilla_master')
 <div class="ui two column doubling stackable grid container">
 	<div class="ui one column centered grid">
 		<div class="column">
@@ -12,14 +13,14 @@
 								        	<input type="text" name="usuario" placeholder="Usuario">
 								      	</div>
 
-								      	<div class="nine wide field">
+								      	<div class="sixteen wide field">
 											<input type="text" name="Email" placeholder="Direccion Email">
 										</div>
 								    </div>
 						  		</div>
 
 								<div class="field">
-								    <div class="two wide fields">
+								    <div class="sixteen wide fields">
 										<div class="field">	
 								        	<input type="password" name="password" placeholder="Password">
 								        </div>
@@ -27,7 +28,32 @@
 								      	<div class="field">
 								        	<input type="password" name="password" placeholder="Confirmar Password">
 								      	</div>
+
+								      	<div class="sixteen wide fields">
+									      	<div class="field">
+										      	<select name="gender" class="ui dropdown" id="select">
+												  	<option value="">Tipo Usuario</option>
+												  	<option value="tipo1">Primero</option>
+												 	<option value="tipo2">Segundo</option>
+												 	<option value="tipo2">Tercero</option>
+												 	<option value="tipo4">Cuarto</option>
+												</select>
+											</div>
+										</div>
 								    </div>
+								</div>
+								<div class="field">
+									<div class="sixteen wide fields">
+										<div class="field">
+									      	<select name="gender" class="ui dropdown" id="select">
+											  	<option value="">Permisos</option>
+											  	<option value="tipo1">PrimerASFASFASFAo</option>
+											 	<option value="tipo2">SeguasdfbasdjkASDndo</option>
+											 	<option value="tipo2">TercaSasfaserasfASFASo</option>
+											 	<option value="tipo4">CuaASaSDASDAISJDAISJDOAISJdoAISJDOAIJSodiaJSODAJSoJDAOSIJDAOSIjdoAISJDOAISJdoaIJSDOAIJSDoaIJSDOAIJSdoIJASOsjdhakjsdbkajshdlkjahslkhsdlkaHSDLKAHSLDkjaHSLKDJHALSKJdhADFAYSaGDKAGHDIAULShdiALUSHDIAUShdIUASHDIAUSdrto</option>
+											</select>
+										</div>
+									</div>
 								</div>
 						</form>
 			  		</div>
@@ -36,3 +62,4 @@
 		</div>
 	</div>
 </div>
+
