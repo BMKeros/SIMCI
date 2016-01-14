bf74ff0b35346e640378168218194d27c81c7c78
daniels <danztdw@gmail.com>
2016-01-14 11:37:41 -0430

    [MOD]Formulario de crear usuario y persona

--- app/views/usuarios/crear_usuario.blade.php
+++ app/views/usuarios/crear_usuario.blade.php
@@ -54,8 +54,78 @@
 											 	<option value="tipo4">Cuarto</option>
 											</select>
 										</div>
+										
+										<div class="ui toggle checkbox">
+  											<input type="checkbox" name="public">
+  												<label>Activar Usuario</label>
+										</div>
+									</div>
+
+									<div class="field">
+										<input type="file" name="imagen" placeholder="">
 									</div>
 								</div>
+						</form>
+			  		</div>
+				</div>
+			</div>
+		</div>
+		
+		<h4 class="ui dividing header">Datos Personales</h4>
+		<div class="column">
+	    	<div class="ten wide column">
+			  	<div class="column">
+					<div class="ui form">
+						<form class="ui form">
+							  	<div class="field">
+								    <div class="fields">
+								      	<div class="eight wide field">
+								        	<input type="text" name="primer_nombre" placeholder="Primer Nombre">
+								      	</div>
+
+								      	<div class="eight wide field">
+											<input type="text" name="segundo_nombre" placeholder="Segundo Nombre">
+										</div>
+								    </div>
+						  		</div>
+
+								<div class="field">
+								    <div class="fields">
+										<div class="eight wide field">	
+								        	<input type="text" name="primer_apellido" placeholder="Primer Apellido">
+								        </div>
+
+								      	<div class="eight wide field">
+								        	<input type="text" name="segundo_apellido" placeholder="Segundo Apellido">
+								      	</div>
+									</div>
+								</div>
+
+								<div class="field">
+									<div class="fields">
+										<div class="nine wide field">
+											<input type="text" name="cedula" placeholder="Cedula">
+										</div>
+
+										<div class="nine wide field">
+											<input type="date" name="fecha_nacimiento" placeholder="Fecha Nacimieto">
+										</div>
+									</div>
+								</div>
+								
+								<div class="field">
+									<div class="fields">
+										<div class="eight wide field">
+											<select class="ui dropdown">
+								  				<option value="">Sexo</option>
+												<option value="1">Masculino</option>
+												<option value="0">Femenino</option>
+											</select>
+										</div>
+									</div>
+								</div>
+
+							<button class=" ui mediun right floated teal submit button"  id="btn-inicio-sesion"><i class="check circle icon"></i>Registrar</button>
 						</form>
 			  		</div>
 				</div>
