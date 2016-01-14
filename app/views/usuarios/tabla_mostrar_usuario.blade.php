2a0e760a38812aba9b8fa06c8629a86bec787eb9
daniels <danztdw@gmail.com>
2016-01-14 15:24:34 -0430

    [MOD] Modificada Tabla de Mostrar usuario, Corrigiendo algunos errores.

--- app/views/usuarios/tabla_mostrar_usuario.blade.php
+++ app/views/usuarios/tabla_mostrar_usuario.blade.php
@@ -1,245 +1,33 @@
 @extends('layouts.plantilla_master')
+@section ('contenido-body-master')
   <div class="ui two column doubling stackable grid container">
-    <div class="ui one column centered grid">
+    <div class="ui container centered grid">
       <div class="column">
         <table class="ui compact celled definition table">
             <thead>
               <tr>
                 <th></th>
-                <th>Fecha de Registro</th>
+                <th>usuario</th>
                 <th>Direccion electronica</th>
-                <th>Ver Usuario</th>
-                <th>Actualizar Ususario</th>
-                <th>Modificar Usuario</th>
+                <th>permiso</th>
+                <th>acciones</th>
+               
               </tr>
             </thead>
             <tbody>
-              <tr>
-                <td>John Lilki</td>
-                <td>September 14, 2013</td>
+              <tr ng-repeat='x in [1,2,3,4,5,6,7,8,9,10]'>
+                <td></td>
+                <td>Daniel Bonalde</td>
                 <td>jhlilk22@yahoo.com</td>
-                <td>
-                  <div class="ui icon button blue"  data-content="Ver Usuario">
+                <td> Tipo de Permiso</td>
+                <td class="thre wide">
+                  <div class="ui icon button blue prueba"  data-content="Ver Usuario">
                       <i class="unhide icon"></i>
                   </div>
-                </td>
-                <td>
-                  <div class="ui icon button green "  data-content="Modificar Usuario">
+                  <div class="ui icon button green prueba"  data-content="Modificar Usuario">
                       <i class="edit icon"></i>
                   </div>
-                </td>
-                <td>
-                  <div class="ui icon button red "  data-content="Eliminar usuario">
-                      <i class="remove icon"></i>
-                  </div>
-                </td>
-              </tr>
-
-              <tr>  
-                <td>Jamie Harington</td>
-                <td>January 11, 2014</td>
-                <td>jamieharingonton@yahoo.com</td>
-                <td>
-                  <div class="ui icon button blue"  data-content="Ver Usuario">
-                      <i class="unhide icon"></i>
-                  </div>
-                </td>
-                <td>
-                  <div class="ui icon button green "  data-content="Modificar Usuario">
-                      <i class="edit icon"></i>
-                  </div>
-                </td>
-                <td>
-                  <div class="ui icon button red "  data-content="Eliminar usuario">
-                      <i class="remove icon"></i>
-                  </div>
-                </td>
-              </tr>
-
-              <tr>
-                <td>Jill Lewis</td>
-                <td>May 11, 2014</td>
-                <td>jilsewris22@yahoo.com</td>
-                <td>
-                  <div class="ui icon button blue"  data-content="Ver Usuario">
-                      <i class="unhide icon"></i>
-                  </div>
-                </td>
-                <td>
-                  <div class="ui icon button green "  data-content="Modificar Usuario">
-                      <i class="edit icon"></i>
-                  </div>
-                </td>
-                <td>
-                  <div class="ui icon button red "  data-content="Eliminar usuario">
-                      <i class="remove icon"></i>
-                  </div>
-                </td>
-              </tr>
-              
-              <tr>
-                <td>Jamie Harington</td>
-                <td>January 11, 2014</td>
-                <td>jamieharingonton@yahoo.com</td>
-                <td>
-                  <div class="ui icon button blue"  data-content="Ver Usuario">
-                      <i class="unhide icon"></i>
-                  </div>
-                </td>
-                <td>
-                  <div class="ui icon button green "  data-content="Modificar Usuario">
-                      <i class="edit icon"></i>
-                  </div>
-                </td>
-                <td>
-                  <div class="ui icon button red "  data-content="Eliminar usuario">
-                      <i class="remove icon"></i>
-                  </div>
-                </td>
-              </tr>
-              
-              <tr>  
-                <td>Jamie Harington</td>
-                <td>January 11, 2014</td>
-                <td>jamieharingonton@yahoo.com</td>
-                <td>
-                  <div class="ui icon button blue"  data-content="Ver Usuario">
-                      <i class="unhide icon"></i>
-                  </div>
-                </td>
-                <td>
-                  <div class="ui icon button green "  data-content="Modificar Usuario">
-                      <i class="edit icon"></i>
-                  </div>
-                </td>
-                <td>
-                  <div class="ui icon button red "  data-content="Eliminar usuario">
-                      <i class="remove icon"></i>
-                  </div>
-                </td>
-              </tr>
-              
-              <tr>
-                <td>Jamie Harington</td>
-                <td>January 11, 2014</td>
-                <td>jamieharingonton@yahoo.com</td>
-                <td>
-                  <div class="ui icon button blue"  data-content="Ver Usuario">
-                      <i class="unhide icon"></i>
-                  </div>
-                </td>
-                <td>
-                  <div class="ui icon button green "  data-content="Modificar Usuario">
-                      <i class="edit icon"></i>
-                  </div>
-                </td>
-                <td>
-                  <div class="ui icon button red "  data-content="Eliminar usuario">
-                      <i class="remove icon"></i>
-                  </div>
-                </td>
-              </tr>
-              
-              <tr>  
-                <td>Jamie Harington</td>
-                <td>January 11, 2014</td>
-                <td>jamieharingonton@yahoo.com</td>
-                <td>
-                  <div class="ui icon button blue"  data-content="Ver Usuario">
-                      <i class="unhide icon"></i>
-                  </div>
-                </td>
-                <td>
-                  <div class="ui icon button green "  data-content="Modificar Usuario">
-                      <i class="edit icon"></i>
-                  </div>
-                </td>
-                <td>
-                  <div class="ui icon button red "  data-content="Eliminar usuario">
-                      <i class="remove icon"></i>
-                  </div>
-                </td>
-              </tr>
-              
-              <tr>  
-                <td>Jamie Harington</td>
-                <td>January 11, 2014</td>
-                <td>jamieharingonton@yahoo.com</td>
-                <td>
-                  <div class="ui icon button blue"  data-content="Ver Usuario">
-                      <i class="unhide icon"></i>
-                  </div>
-                </td>
-                <td>
-                  <div class="ui icon button green "  data-content="Modificar Usuario">
-                      <i class="edit icon"></i>
-                  </div>
-                </td>
-                <td>
-                  <div class="ui icon button red "  data-content="Eliminar usuario">
-                      <i class="remove icon"></i>
-                  </div>
-                </td>
-              </tr>
-              
-              <tr>  
-                <td>Jamie Harington</td>
-                <td>January 11, 2014</td>
-                <td>jamieharingonton@yahoo.com</td>
-                <td>
-                  <div class="ui icon button blue"  data-content="Ver Usuario">
-                      <i class="unhide icon"></i>
-                  </div>
-                </td>
-                <td>
-                  <div class="ui icon button green "  data-content="Modificar Usuario">
-                      <i class="edit icon"></i>
-                  </div>
-                </td>
-                <td>
-                  <div class="ui icon button red "  data-content="Eliminar usuario">
-                      <i class="remove icon"></i>
-                  </div>
-                </td>
-              </tr>
-              
-              <tr>  
-                <td>Jamie Harington</td>
-                <td>January 11, 2014</td>
-                <td>jamieharingonton@yahoo.com</td>
-               <td>
-                  <div class="ui icon button blue"  data-content="Ver Usuario">
-                      <i class="unhide icon"></i>
-                  </div>
-                </td>
-                <td>
-                  <div class="ui icon button green "  data-content="Modificar Usuario">
-                      <i class="edit icon"></i>
-                  </div>
-                </td>
-                <td>
-                  <div class="ui icon button red "  data-content="Eliminar usuario">
-                      <i class="remove icon"></i>
-                  </div>
-                </td>
-              </tr>
-              
-              <tr>  
-                <td>Jamie Harington</td>
-                <td>January 11, 2014</td>
-                <td>jamieharingonton@yahoo.com</td>
-               <td>
-                  <div class="ui icon button blue"  data-content="Ver Usuario">
-                      <i class="unhide icon"></i>
-                  </div>
-                </td>
-                <td>
-                  <div class="ui icon button green "  data-content="Modificar Usuario">
-                      <i class="edit icon"></i>
-                  </div>
-                </td>
-                <td>
-                  <div class="ui icon button red "  data-content="Eliminar usuario">
+                  <div class="ui icon button red prueba"  data-content="Eliminar Usuario">
                       <i class="remove icon"></i>
                   </div>
                 </td>
@@ -249,3 +37,15 @@
       </div>
     </div>
   </div>
+  @stop
+
+@section('js')
+<script>
+$(document).ready(function(){
+$('.prueba')
+    .popup();
+});
+  
+
+</script>
+@stop
