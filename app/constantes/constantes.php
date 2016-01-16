27f0e13f083ac1759fc01c96ac097ed03ac5e8ff
Israel Lugo <hostelixisrael@gmail.com>
2016-01-15 22:27:06 -0430

    [MOD] agregada nueva constante al archivo la cual tiene la ruta de las imagenes perfil

--- app/constantes/constantes.php
+++ app/constantes/constantes.php
@@ -8,3 +8,8 @@ define('TIPO_USER_PROFESOR','TU03');
 define('TIPO_USER_ESTUDIANTE','TU04');
 define('TIPO_USER_ALMACENISTA','TU05');
 define('TIPO_USER_SUPERVISOR','TU06');
+
+
+/// PATH
+
+define('PATH_IMAGENES','/uploads/imagenes/');
