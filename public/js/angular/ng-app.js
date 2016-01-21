f1447f2e414319cd14af398b7e94f646de7ff5d6
Israel Lugo <hostelixisrael@gmail.com>
2016-01-21 09:50:12 -0430

    [MOD] agregada la directiva de datatables

--- public/js/angular/ng-app.js
+++ public/js/angular/ng-app.js
@@ -1,5 +1,5 @@
 (function(){
-  var simci = angular.module('SIMCI', ['ngRoute'], function($interpolateProvider) {
+  var simci = angular.module('SIMCI', ['ngRoute','datatables'], function($interpolateProvider) {
           $interpolateProvider.startSymbol('<%');
           $interpolateProvider.endSymbol('%>');
   });
