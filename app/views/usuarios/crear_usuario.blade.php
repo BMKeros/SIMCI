e96e9193cc2fba6620d1c114b7da7e495260f810
Israel Lugo <hostelixisrael@gmail.com>
2016-01-22 12:06:12 -0430

    [MOD] modificaciones minimas en la vista de usuarios

--- app/views/usuarios/crear_usuario.blade.php
+++ app/views/usuarios/crear_usuario.blade.php
@@ -16,7 +16,7 @@
 			</div>
 			
 			<form class="ui form" id="formulario_crear_usuario">
-				<h3 class="ui centered dividing header">Datos de usuario</h3>
+				<h3 class="ui centered dividing header">Datos de Usuario</h3>
 				
 				<div class="field">
 					<div class="two fields">
