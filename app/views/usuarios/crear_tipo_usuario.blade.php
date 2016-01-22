8b6e709a83d5fb3e1fbd06f3a172fff55e10ac49
danzt <danztdw@gmail.com>
2016-01-22 11:00:14 -0430

    [MOD] Edite la vista de tipo de usuario

---
+++ app/views/usuarios/crear_tipo_usuario.blade.php
@@ -0,0 +1,28 @@
+<div class="ui centered grid">
+	<div class="six wide tablet twelve wide computer column">
+		<div class="ui form">
+			<form class="ui form" id="formulario_crear_permisos_usuarios">
+				<h3 class="ui centered dividing header">Crear Tipo de Usuario</h3>
+
+				<div class="field">
+					<div class="six wide field">
+						<div class="field">
+							<label>Tipo de usuario</label>
+			        		<input type="text" name="tipo_usuario" value="" placeholder="Tipo de Usuario">
+			        	</div>
+			        </div>
+		        </div>
+
+		        <div class="field">
+		        	<div class="nine wide field ui form">
+					  	<div class="field">
+					    	<label>Descripcion</label>
+					    		<textarea></textarea>
+					  	</div>
+					</div>
+		        </div>
+			</form>
+		</div>
+	</div>
+</div>
+	
