6c752807fcdfe8668909ec148124cb886d460c3f
Israel Lugo <hostelixisrael@gmail.com>
2016-01-22 01:29:12 -0430

    [MOD] modificada l avista de crear objeto añadidod los ng-model y algunas cosas mas

--- app/views/catalogo/crear_objeto.blade.php
+++ app/views/catalogo/crear_objeto.blade.php
@@ -1,6 +1,20 @@
 <div class="ui centered grid">
 	<div class="six wide tablet twelve wide computer column">
 		<div class="ui form">
+			
+			<div ng-if="mostrar_mensaje">
+				<div class="ui icon <% mensaje_validacion.color %> message">
+					<i class="<% mensaje_validacion.icono %> icon"></i>
+					<div class="content">
+						<div class="header"><% mensaje_validacion.titulo %></div>
+						<ul class="list">
+							<li ng-repeat=" mensaje in mensaje_validacion.mensajes track by $index"><% mensaje | capitalize %></li>
+						</ul>
+					</div>
+				</div>
+				<br>
+			</div>
+
 			<form class="ui form" id="formulario_crear_objeto">
 				<h3 class="ui centered dividing header">Registrar Objeto</h3>
 				<br>
@@ -8,7 +22,7 @@
 					<div class="three fields">
 						<div class="field">
 							<label>Nombre del Producto</label>
-			        		<input type="text" name="nombre_pruducto" placeholder="Nombre">
+			        		<input type="text" name="nombre_objeto" placeholder="Nombre" ng-model="DatosForm.nombre">
 			        	</div>
 				    </div>
 				</div>
@@ -18,12 +32,12 @@
 			      		
 						 	<div class="field">
 						    	<label>Especificacion del Producto</label>
-						    	<textarea></textarea>
+						    	<textarea ng-model="DatosForm.especificaciones"></textarea>
 						  	</div>
 						  	
 						  	<div class="field">
 							    <label>Descripcion del Producto</label>
-							    <textarea></textarea>
+							    <textarea ng-model="DatosForm.descripcion"></textarea>
 							</div>
 						
 				    </div>
@@ -35,19 +49,19 @@
 				    <div class="two fields">
 				      	<div class="field">
 				      		<label>Unidad</label>
-				      		{{ Form::select_unidades (array('name'=>'cod_unidad', 'id'=>'unidad','ng-modal'=>'unidad'))}}
+				      		{{ Form::select_unidades (array('name'=>'cod_unidad', 'id'=>'unidad','ng-modal'=>'DatosForm.cod_unidad'))}}
 			     		</div>
 
 			     		<div class="field">
 			     			<label>Tipo de Objeto</label>
-			     			{{ Form::select_agrupacion(array('name'=>'cod_tipo_objeto', 'id'=>'tipo_objeto', 'ng-modal'=>'tipo_objeto')) }}
+			     			{{ Form::select_agrupacion(array('name'=>'cod_tipo_objeto', 'id'=>'tipo_objeto', 'ng-modal'=>'DatosForm.cod_tipo_objeto')) }}
 						</div>
 				    </div>
 				</div>
 			
 			<br>
 
-			<button type="submit" class="ui big right floated submit button blue" class=" ng-click="registrar_objeto()"">Registrar</button>
+			<button type="submit" class="ui big right floated submit button blue" ng-click="registrar_objeto()" id="btn-registrar">Registrar</button>
 			</form>
 		</div>
 	</div>
