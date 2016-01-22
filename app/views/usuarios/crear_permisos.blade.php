c7d230a3534d3e324917d9ddee5b4fc55ed4e748
danzt <danztdw@gmail.com>
2016-01-22 09:36:29 -0430

    [ADD]Vista de crear permisos del usuario

---
+++ app/views/usuarios/crear_permisos.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.plantilla_master')
+@section('contenido-body-master')
+<div class="ui centered grid">
+	<div class="six wide tablet twelve wide computer column">
+		<div class="ui form">
+			<form class="ui form" id="formulario_crear_permisos_usuarios">
+				<h3 class="ui centered dividing header">Permisos de Usuario</h3>
+
+				<div class="field centered grid">
+					<div class="two fields">
+						<div class="field">
+							<label>Permisos del Usuario</label>
+							<input type="text" name="permiso"placeholder="Nombre del Permiso">
+			        		
+			        	</div>
+			        	<div class="field">
+				      		<label>Codigo</label>	
+				        	<input type="text" name="cod_permiso" placeholder="Codigo del Permiso">
+				      	</div>
+				    </div>
+
+				    <div class="field">
+				    	<label>Descripcion de Permiso</label>
+				    	<textarea "ng-modal Datos.Form.Descripcion" placeholder="Descripcion de los Permisos"></textarea>
+				    </div>
+				</div>
+
+				<div class="ui big right floated submit button blue" ng-click="registrar_objeto()" id="btn-registrar"> Aceptar
+				</div>
+			</form>
+		</div>
+	</div>
+</div>
+@stop
