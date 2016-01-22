2d79f3a4d9b72179ff55ae1bc4c9973eb94f8328
Israel Lugo <hostelixisrael@gmail.com>
2016-01-22 01:29:55 -0430

    [MOD] modificaciones en el controlador de catalogo primeros pasos para el guardado en la db

--- public/js/angular/controladores/catalogo_controller.js
+++ public/js/angular/controladores/catalogo_controller.js
@@ -28,6 +28,72 @@ simci.controller('CatalogoController', ['$scope','$http','$log','$route', '$rout
     
     $log.info($routeParams);
     $log.info($location);
+
+    if($location.$$url == '/catalogo/crear-objeto'){
+
+      $scope.mostrar_mensaje = false;
+
+        $scope.registrar_objeto = function(){
+        
+         /* var formulario = $('#formulario_crear_objeto');
+          var is_valid_form = formulario.form(reglas_formulario_crear).form('is valid');
+
+          if(is_valid_form){
+            
+            //Activamos el loading
+            $('#btn-registrar').addClass('loading').prop('disabled',true);
+
+            $http({
+              method: 'POST',
+              url: '/api/catalogo/registrar-objeto',
+              data: $scope.DatosForm
+            }).then(function(data){
+
+              if(data.data.resultado){
+                
+                $scope.mostrar_mensaje = true;
+                $scope.mensaje_validacion = {
+                  titulo: 'Objeto creado con exito',
+                  icono: 'checkmark',
+                  color: 'green',
+                  mensajes: ['El Objeto ha sido agregado al catalogo.']
+                };
+
+                $timeout(function(){
+                  //Desactivamos el loading
+                  $('#btn-registrar').removeClass('loading').prop('disabled',false);
+                  formulario.form('clear');
+                }, 0, false);
+
+              }
+              else{
+
+                $scope.mostrar_mensaje = true;
+                $scope.mensaje_validacion = {
+                  titulo: 'Hubo un error al guardar la informacion',
+                  icono: 'remove',
+                  color: 'red',
+                  mensajes: data.data.mensajes
+                };
+              }
+
+              //Desactivamos el loading
+              $('#btn-registrar').removeClass('loading').prop('disabled',false);
+
+            },function(data_error){
+
+              console.log(data_error);
+
+              //Desactivamos el loading
+              $('#btn-registrar').removeClass('loading').prop('disabled',false);
+            });
+            
+          } //If condicional*/
+
+          $log.info($scope.DatosForm);
+        }
+    
+      }// If == '/usuarios/crear'
     
   }]
 );
