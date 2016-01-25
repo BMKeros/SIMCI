5b04f32c0b04b53fb8ef9b2326db6a710555a70c
Israel Lugo <hostelixisrael@gmail.com>
2016-01-25 12:17:54 -0430

    [MOD] modificado el unidadestableseeder adaptado al cambio de la tabla unidades

--- app/database/seeds/UnidadesTableSeeder.php
+++ app/database/seeds/UnidadesTableSeeder.php
@@ -8,11 +8,10 @@ class UnidadesTableSeeder extends Seeder {
        DB::table('unidades')->delete();
 
         $campos = array(
-        	array('cod_unidad' => '1', 'descripcion' => 'mililitros', 'abreviatura'=>'ML'),
-        	array('cod_unidad' => '2', 'descripcion' => 'miligramos', 'abreviatura'=>'MG'),
-        	array('cod_unidad' => '3', 'descripcion' => 'gramos', 'abreviatura'=>'G'),
-        	array('cod_unidad' => '4', 'descripcion' => 'kilogramos', 'abreviatura'=>'KG'),
-            
+        	array('cod_unidad' => '1', 'nombre' => 'mililitros', 'abreviatura'=>'ML'),
+        	array('cod_unidad' => '2', 'nombre' => 'miligramos', 'abreviatura'=>'MG'),
+        	array('cod_unidad' => '3', 'nombre' => 'gramos', 'abreviatura'=>'G'),
+        	array('cod_unidad' => '4', 'nombre' => 'kilogramos', 'abreviatura'=>'KG'),
         );
 
         DB::table('unidades')->insert($campos);
