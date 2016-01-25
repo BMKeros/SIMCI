7ddff45814f540107a5d81421df27b10e64969d3
Israel Lugo <hostelixisrael@gmail.com>
2016-01-25 12:14:04 -0430

    [MOD] modificaciones en los modelos adaptados a las consultas json, ahora se llama data_ en vez de attr

--- app/models/Persona.php
+++ app/models/Persona.php
@@ -4,6 +4,7 @@ class Persona Extends Eloquent{
 
 	protected $fillable = array('primer_nombre', 'segundo_nombre', 'primer_apellido', 'segundo_apellido', 'cedula', 'usuario_id','sexo_id', 'fecha_nacimiento');
 
+	protected $visible =  array('id','primer_nombre', 'segundo_nombre', 'primer_apellido', 'segundo_apellido', 'cedula', 'usuario_id','sexo_id', 'fecha_nacimiento','data_sexo');
 
 	public function usuario(){
 		return $this->belongsTo('Usuario');
@@ -13,12 +14,12 @@ class Persona Extends Eloquent{
 		return $this->belongsTo('Sexo');
 	}
 
-	public function getAttrSexoAttribute()
+	public function getDataSexoAttribute()
     {
         return $this->sexo->toArray();
     }
 
-    protected $appends = ['attr_sexo'];
+    protected $appends = ['data_sexo'];
 }
 
 ?>
