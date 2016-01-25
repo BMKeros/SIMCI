131824d446c057ee4b4c2e09c0267f31f90e3164
Israel Lugo <hostelixisrael@gmail.com>
2016-01-25 12:12:35 -0430

    [MOD] modificado modelo catalogo se le agrego las data_unidad la cual trae todos los campos de la relacion respectiva, y se le agrego la variable

--- app/models/Catalogo.php
+++ app/models/Catalogo.php
@@ -2,6 +2,18 @@
 class Catalogo extends Eloquent{
 	protected $table = 'catalogo_objetos';
 	protected $fillable = array('nombre', 'descripcion', 'especificaciones', 'cod_unidad', 'cod_tipo_objeto');
+	protected $visible = array('id','nombre', 'descripcion', 'especificaciones', 'cod_unidad', 'cod_tipo_objeto','data_unidad');
+
+	public function unidad(){
+		return $this->belongsTo('Unidad','cod_unidad');
+	}
+
+	public function getDataUnidadAttribute()
+    {
+        return $this->unidad->toArray();
+    }
+
+    protected $appends = array('data_unidad');
 }
 
 ?>
