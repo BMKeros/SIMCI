d02474f69ee7664d03dc33d47e4617b29ffc4c06
Israel Lugo <hostelixisrael@gmail.com>
2016-01-26 10:21:25 -0430

    [MOD] modificacinoes en el modelo tipo objeto quitado lineas inncesarias, modificado el modelo de usuario agregada la relacion con tipo de usuario

--- app/models/TiposUsuario.php
+++ app/models/TiposUsuario.php
@@ -1,27 +1,11 @@
 <?php
 
-use Illuminate\Auth\UserTrait;
-use Illuminate\Auth\UserInterface;
-use Illuminate\Auth\Reminders\RemindableTrait;
-use Illuminate\Auth\Reminders\RemindableInterface;
+class TiposUsuario extends Eloquent {
 
-class TiposUsuario extends Eloquent implements UserInterface, RemindableInterface {
-
-	use UserTrait, RemindableTrait;
-
-	/**
-	 * The database table used by the model.
-	 *
-	 * @var string
-	 */
 	protected $table = 'tipos_usuario';
 	protected $fillable = array('codigo', 'descripcion');
+	protected $primaryKey = 'codigo';
 
-	/**
-	 * The attributes excluded from the model's JSON form.
-	 *
-	 * @var array
-	 */
 
 	public function usuarios()
     {
