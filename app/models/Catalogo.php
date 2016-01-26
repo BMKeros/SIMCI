3de3fcecb5074fa87ab2e56e19cf6ad4b9ee76c7
Israel Lugo <hostelixisrael@gmail.com>
2016-01-26 01:00:58 -0430

    [MOD] agregada la relacion de tipo_objeto

--- app/models/Catalogo.php
+++ app/models/Catalogo.php
@@ -2,10 +2,14 @@
 class Catalogo extends Eloquent{
 	protected $table = 'catalogo_objetos';
 	protected $fillable = array('nombre', 'descripcion', 'especificaciones', 'cod_unidad', 'cod_tipo_objeto');
-	protected $visible = array('id','nombre', 'descripcion', 'especificaciones', 'cod_unidad', 'cod_tipo_objeto','data_unidad');
+	protected $visible = array('id','nombre', 'descripcion', 'especificaciones', 'cod_unidad', 'cod_tipo_objeto','data_unidad','data_tipo_objeto');
 
 	public function unidad(){
 		return $this->belongsTo('Unidad','cod_unidad');
+	}
+
+	public function tipoObjeto(){
+		return $this->belongsTo('TipoObjeto','cod_tipo_objeto');
 	}
 
 	public function getDataUnidadAttribute()
@@ -13,7 +17,12 @@ class Catalogo extends Eloquent{
         return $this->unidad->toArray();
     }
 
-    protected $appends = array('data_unidad');
+    public function getDataTipoObjetoAttribute()
+    {
+        return $this->tipoObjeto;
+    }
+
+    protected $appends = array('data_unidad', 'data_tipo_objeto');
 }
 
 ?>
