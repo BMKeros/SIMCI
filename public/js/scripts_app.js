517e1db27192524e3d3e4a07ac3cbdc6ca1f06d5
Israel Lugo <hostelixisrael@gmail.com>
2016-01-27 10:04:59 -0430

    [MOD] agregada la funcion cut_string a TOOLS_APP

--- public/js/scripts_app.js
+++ public/js/scripts_app.js
@@ -6,6 +6,10 @@
         angular.element('#reloj').text(moment().format('h:mm:ss A'))
       },1000);
     },
+    cut_string: function(string, num_char){
+      var infin = ((string.length > num_char)?'....':'');
+      return string.substring(0,num_char)+infin;
+    },
   };
 
   window.TOOLS_APP = TOOLS;
