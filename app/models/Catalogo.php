5f7cad35ae2255f56b6a18ca3ed48b2932599392
Israel Lugo <hostelixisrael@gmail.com>
2016-01-29 09:31:13 -0430

    [MOD] agregadas las modificaciones en clase objetos para el modelo catalogo

--- app/models/Catalogo.php
+++ app/models/Catalogo.php
@@ -2,14 +2,14 @@
 class Catalogo extends Eloquent{
 	protected $table = 'catalogo_objetos';
 	protected $fillable = array('nombre', 'descripcion', 'especificaciones', 'cod_unidad', 'cod_tipo_objeto');
-	protected $visible = array('id','nombre', 'descripcion', 'especificaciones', 'cod_unidad', 'cod_tipo_objeto','data_unidad','data_tipo_objeto');
+	protected $visible = array('id','nombre', 'descripcion', 'especificaciones', 'cod_unidad', 'cod_tipo_objeto','data_unidad','data_clase_objeto');
 
 	public function unidad(){
 		return $this->belongsTo('Unidad','cod_unidad');
 	}
 
-	public function tipoObjeto(){
-		return $this->belongsTo('TipoObjeto','cod_tipo_objeto');
+	public function claseObjeto(){
+		return $this->belongsTo('ClaseObjeto','cod_clase_objeto');
 	}
 
 	public function getDataUnidadAttribute()
@@ -17,12 +17,12 @@ class Catalogo extends Eloquent{
         return $this->unidad->toArray();
     }
 
-    public function getDataTipoObjetoAttribute()
+    public function getDataClaseObjetoAttribute()
     {
-        return $this->tipoObjeto;
+        return $this->claseObjeto;
     }
 
-    protected $appends = array('data_unidad', 'data_tipo_objeto');
+    protected $appends = array('data_unidad', 'data_clase_objeto');
 }
 
 ?>
