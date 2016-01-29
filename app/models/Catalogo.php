b70c0d0e202d445e84b3beb2b4cc31697da063fb
Israel Lugo <hostelixisrael@gmail.com>
2016-01-29 10:26:55 -0430

    [MOD] modificado el modelo de catalogo porque no se habia agregado el campo de cod_clase_objeto al fillable

--- app/models/Catalogo.php
+++ app/models/Catalogo.php
@@ -1,8 +1,8 @@
 <?php  
 class Catalogo extends Eloquent{
 	protected $table = 'catalogo_objetos';
-	protected $fillable = array('nombre', 'descripcion', 'especificaciones', 'cod_unidad', 'cod_tipo_objeto');
-	protected $visible = array('id','nombre', 'descripcion', 'especificaciones', 'cod_unidad', 'cod_tipo_objeto','data_unidad','data_clase_objeto');
+	protected $fillable = array('nombre', 'descripcion', 'especificaciones', 'cod_unidad', 'cod_clase_objeto');
+	protected $visible = array('id','nombre', 'descripcion', 'especificaciones', 'cod_unidad', 'cod_clase_objeto','data_unidad','data_clase_objeto');
 
 	public function unidad(){
 		return $this->belongsTo('Unidad','cod_unidad');
