cc0544f944d22e2352711313263a05784e411fad
Israel Lugo <hostelixisrael@gmail.com>
2016-01-29 10:05:56 -0430

    [MOD] modificado la migracion de tipo objeto por la clase objetos

---
+++ app/database/migrations/2016_01_05_234246_crear_tabla_clase_objetos.php
@@ -0,0 +1,34 @@
+g<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CrearTablaTipoObjetos extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('clase_objetos', function($table)
+		{
+			$table->increments('id');
+			$table->string('nombre',30);
+			$table->string('descripcion',50);
+			$table->nullableTimestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		drop_cascade('clase_objetos');
+	}
+
+}
