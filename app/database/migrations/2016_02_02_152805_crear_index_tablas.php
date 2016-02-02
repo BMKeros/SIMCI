585f00676e66222272a38d74735c459e7962beb1
ivantelix <ivantelix@gmail.com>
2016-02-02 10:59:52 -0430

    [ADD]creado nuevas migraciones mensajes y notificaciones y [MOD]recreada la migracion de index para que quede de ultimo

---
+++ app/database/migrations/2016_02_02_152805_crear_index_tablas.php
@@ -0,0 +1,86 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CrearIndexTablas extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		//ELEMENTOS QUIMICOS
+		Schema::table('elementos_quimicos', function($table){
+    		$table->foreign('cod_estado')->references('cod_estado')
+    			->on('estados_materia');
+    			//->onDelete('cascade');
+    		$table->foreign('cod_clasificacion')->references('cod_clasificacion')
+    			->on('clasificacion_elementos');
+    			//->onDelete('cascade');
+    		$table->foreign('cod_subclasificacion')->references('cod_subclasificacion')
+    			->on('subclasificacion_elementos');
+    			//->onDelete('cascade');
+		});
+
+		//USUARIOS
+		Schema::table('usuarios', function($table){
+		
+			$table->foreign('cod_tipo_usuario')->references('codigo')
+				->on('tipos_usuario');
+				//->onDelete('cascade');
+		});
+
+		//PERSONAS
+		Schema::table('personas', function($table){
+
+			$table->foreign('usuario_id')->references('id')
+				->on('usuarios')
+				->onDelete('cascade');
+
+			$table->foreign('sexo_id')->references('id')
+				->on('sexos');
+				//->onDelete('cascade');
+		});
+
+		//ALMACENES
+		Schema::table('almacenes', function($table){
+			$table->foreign('responsable')->references('id')
+				->on('usuarios');
+				//->onDelete('cascade');
+
+			$table->foreign('primer_auxiliar')->references('id')
+				->on('personas');
+				//->onDelete('cascade');
+
+			$table->foreign('segundo_auxiliar')->references('id')
+				->on('personas');
+				//->onDelete('cascade');
+		});
+
+		//CATALOGO
+		Schema::table('catalogo_objetos', function($table){
+			$table->foreign('cod_unidad')->references('cod_unidad')
+				->on('unidades');
+				//->obDelete('cascade');
+			$table->foreign('cod_clase_objeto')->references('id')
+				->on('clase_objetos');
+				//->onDelete('cascade');
+		});
+	}	
+
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+
+	}
+
+}
