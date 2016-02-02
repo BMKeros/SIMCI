5cbf87f6fce6b3854dd7f5bd894923a82441f5d9
Israel Lugo <hostelixisrael@gmail.com>
2016-02-02 08:38:09 -0430

    [MOD] modificaciones en la vista de crear objeto, se agrago el mensaje de valdacion y se corrigieron los campos

--- app/views/usuarios/crear_permisos.blade.php
+++ app/views/usuarios/crear_permisos.blade.php
@@ -1,29 +1,38 @@
 <div class="ui centered grid">
 	<div class="six wide tablet twelve wide computer column">
 		<div class="ui form">
-			<form class="ui form" id="formulario_crear_permisos_usuarios">
+			
+			<div ng-if="mostrar_mensaje">
+				<div class="ui icon <% mensaje_validacion.color %> message">
+					<i class="<% mensaje_validacion.icono %> icon"></i>
+					<div class="content">
+						<div class="header"><% mensaje_validacion.titulo %></div>
+						<ul class="list">
+							<li ng-repeat=" mensaje in mensaje_validacion.mensajes track by $index"><% mensaje | capitalize %></li>
+						</ul>
+					</div>
+				</div>
+				<br>
+			</div>
+
+			<form class="ui form" id="formulario_crear_permiso">
 				<h3 class="ui centered dividing header">Crear Permisos</h3>
 
 				<div class="field centered grid">
 					<div class="two fields">
 						<div class="field">
 							<label>Nombre del Permiso</label>
-							<input type="text" name=" permiso"placeholder="Nombre del Permiso">
-			        		
+							<input type="text" name="nombre" placeholder="Nombre del Permiso" ng-model="DatosForm.nombre">
 			        	</div>
-			        	<div class="field">
-				      		<label>Codigo</label>	
-				        	<input type="text" name="cod_permiso" placeholder="Codigo del Permiso">
-				      	</div>
 				    </div>
 
 				    <div class="field">
 				    	<label>Descripcion de Permiso</label>
-				    	<textarea name="descripcion_permiso" "ng-modal Datos.Form.Descripcion" placeholder="Descripcion de Permiso"></textarea>
+				    	<textarea name="descripcion" placeholder="Descripcion de Permiso" ng-model="DatosForm.descripcion"></textarea>
 				    </div>
 				</div>
 
-				<div class="ui big right floated submit button blue" ng-click="registrar_objeto()" id="btn-registrar"> Registrar
+				<div class="ui big right floated submit button blue" ng-click="registrar_permiso()" id="btn-registrar"> Registrar
 				</div>
 			</form>
 		</div>
