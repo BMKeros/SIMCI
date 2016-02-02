c9c3357ee911c41614e3a603a54cd3ecad9c3a65
ivantelix <ivantelix@gmail.com>
2016-02-02 10:32:10 -0430

    [MOD] modificado seeder para agregarle el campo nombre

--- app/database/seeds/TiposUsuarioTableSeeder.php
+++ app/database/seeds/TiposUsuarioTableSeeder.php
@@ -7,12 +7,12 @@ class TiposUsuarioTableSeeder extends Seeder {
        DB::table('tipos_usuario')->delete();
 
         $campos = array(
-        	array('codigo' => TIPO_USER_ROOT, 'descripcion' => 'Root'),
-        	array('codigo' => TIPO_USER_ADMIN, 'descripcion' => 'Administrador'),
-        	array('codigo' => TIPO_USER_PROFESOR, 'descripcion' => 'Profesor'),
-        	array('codigo' => TIPO_USER_ESTUDIANTE, 'descripcion' => 'Estudiante'),
-        	array('codigo' => TIPO_USER_ALMACENISTA, 'descripcion' => 'Almacenista'),
-        	array('codigo' => TIPO_USER_SUPERVISOR, 'descripcion' => 'Supervisor'),
+        	array('codigo' => TIPO_USER_ROOT, 'nombre' => 'Root', 'descripcion' => 'Root'),
+        	array('codigo' => TIPO_USER_ADMIN, 'nombre' => 'Administrador', 'descripcion' => 'Administrador'),
+        	array('codigo' => TIPO_USER_PROFESOR, 'nombre' => 'Profesor', 'descripcion' => 'Profesor'),
+        	array('codigo' => TIPO_USER_ESTUDIANTE, 'nombre' => 'Estudiante', 'descripcion' => 'Estudiante'),
+        	array('codigo' => TIPO_USER_ALMACENISTA, 'nombre' => 'Almacenista', 'descripcion' => 'Almacenista'),
+        	array('codigo' => TIPO_USER_SUPERVISOR, 'nombre' => 'Supervisor', 'descripcion' => 'Supervisor'),
         );
 
         DB::table('tipos_usuario')->insert($campos);
