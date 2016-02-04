72fe875813b65371b44b7a22b8552c2c3b3b82d6
ivantelix <ivantelix@gmail.com>
2016-02-04 10:29:33 -0430

    [MOD]arreglado estetica del seede de unidades

--- app/database/seeds/UnidadesTableSeeder.php
+++ app/database/seeds/UnidadesTableSeeder.php
@@ -10,24 +10,24 @@ class UnidadesTableSeeder extends Seeder {
         $campos = array(
         	array( "nombre"=>'metro', "abreviatura"=>'m', "tipo_unidad"=>'1'),
         	array( "nombre"=>'Pulgada', "abreviatura"=>'plg', "tipo_unidad"=>'1'),
-        	array( "nombre"=>'Pie', "abreviatura"=>'ft', "tipo_unidad"=>'1
-			'),array( "nombre"=>'Micro', "abreviatura"=>'µ', "tipo_unidad"=>'1
-			'),array( "nombre"=>'Decimetro', "abreviatura"=>'dc', "tipo_unidad"=>'1
-			'),array( "nombre"=>'Centimetro', "abreviatura"=>'cm', "tipo_unidad"=>'1
-			'),array( "nombre"=>'Milimetro', "abreviatura"=>'mm', "tipo_unidad"=>'1
-			'),array( "nombre"=>'Micrometro', "abreviatura"=>'µm', "tipo_unidad"=>'1
-			'),array( "nombre"=>'Manometro', "abreviatura"=>'nm', "tipo_unidad"=>'1
-			'),array( "nombre"=>'Picometro', "abreviatura"=>'pm', "tipo_unidad"=>'1
-			'),array( "nombre"=>'Libra', "abreviatura"=>'lb', "tipo_unidad"=>'2
-			'),array( "nombre"=>'Onza', "abreviatura"=>'oz', "tipo_unidad"=>'2
-			'),array( "nombre"=>'Decigramo', "abreviatura"=>'dg', "tipo_unidad"=>'2
-			'),array( "nombre"=>'Centigrados', "abreviatura"=>'cg', "tipo_unidad"=>'2
-			'),array( "nombre"=>'Miligramos', "abreviatura"=>'mg', "tipo_unidad"=>'2
-			'),array( "nombre"=>'Microgramos', "abreviatura"=>'µg', "tipo_unidad"=>'2
-			'),array( "nombre"=>'Manogramos', "abreviatura"=>'ng', "tipo_unidad"=>'2
-			'),array( "nombre"=>'Picogramos', "abreviatura"=>'pg', "tipo_unidad"=>'2
-			'),array( "nombre"=>'Decagramos', "abreviatura"=>'deg', "tipo_unidad"=>'2
-			'),array( "nombre"=>'Hectogramos', "abreviatura"=>'Hg', "tipo_unidad"=>'2'),
+        	array( "nombre"=>'Pie', "abreviatura"=>'ft', "tipo_unidad"=>'1'),
+        	array( "nombre"=>'Micro', "abreviatura"=>'µ', "tipo_unidad"=>'1'),
+        	array( "nombre"=>'Decimetro', "abreviatura"=>'dc', "tipo_unidad"=>'1'),
+        	array( "nombre"=>'Centimetro', "abreviatura"=>'cm', "tipo_unidad"=>'1'),
+        	array( "nombre"=>'Milimetro', "abreviatura"=>'mm', "tipo_unidad"=>'1'),
+        	array( "nombre"=>'Micrometro', "abreviatura"=>'µm', "tipo_unidad"=>'1'),
+        	array( "nombre"=>'Manometro', "abreviatura"=>'nm', "tipo_unidad"=>'1'),
+        	array( "nombre"=>'Picometro', "abreviatura"=>'pm', "tipo_unidad"=>'1'),
+        	array( "nombre"=>'Libra', "abreviatura"=>'lb', "tipo_unidad"=>'2'),
+        	array( "nombre"=>'Onza', "abreviatura"=>'oz', "tipo_unidad"=>'2'),
+        	array( "nombre"=>'Decigramo', "abreviatura"=>'dg', "tipo_unidad"=>'2'),
+        	array( "nombre"=>'Centigrados', "abreviatura"=>'cg', "tipo_unidad"=>'2'),
+        	array( "nombre"=>'Miligramos', "abreviatura"=>'mg', "tipo_unidad"=>'2'),
+        	array( "nombre"=>'Microgramos', "abreviatura"=>'µg', "tipo_unidad"=>'2'),
+        	array( "nombre"=>'Manogramos', "abreviatura"=>'ng', "tipo_unidad"=>'2'),
+        	array( "nombre"=>'Picogramos', "abreviatura"=>'pg', "tipo_unidad"=>'2'),
+        	array( "nombre"=>'Decagramos', "abreviatura"=>'deg', "tipo_unidad"=>'2'),
+        	array( "nombre"=>'Hectogramos', "abreviatura"=>'Hg', "tipo_unidad"=>'2'),
 			array( "nombre"=>'Kilogramos', "abreviatura"=>'Kg', "tipo_unidad"=>'2'),
 			array( "nombre"=>'Miriagramos', "abreviatura"=>'Mig', "tipo_unidad"=>'2'),
 			array( "nombre"=>'Fahreheint', "abreviatura"=>'F', "tipo_unidad"=>'3'),
