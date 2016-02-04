947bb747de47429c7eae2be372bee80ea9b16731
ivantelix <ivantelix@gmail.com>
2016-02-04 10:20:19 -0430

    [MOD]cambiado los nombres a las migraciones de mensajes y notificaciones, [ADD]creado migraciones de laboratorios y de stock de laboratorios y la de los index para que quede de ultima

---
+++ app/database/migrations/2016_02_04_143638_crear_tabla_objetos_laboratorio.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ObjetosLaboratorio extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('objetos_laboratorio', function($table)
+		{
+			$table->increments('id');
+			$table->string('cod_laboratorio');
+			$table->integer('cod_objeto');
+
+			//este campo esta tentativo
+			$table->integer('cod_clase_objeto');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('objetos_laboratorio');
+	}
+
+}
