e91bb5d2bad8e0b18841f7cd1b42ee86053057f3
ivantelix <ivantelix@gmail.com>
2016-02-04 13:00:44 -0430

    [MOD]cambiado

--- app/models/Usuario.php
+++ app/models/Usuario.php
@@ -28,7 +28,7 @@ class Usuario extends Eloquent implements UserInterface, RemindableInterface {
     	return $this->belongsTo('TiposUsuario', 'cod_tipo_usuario');
     }
     public function nombre_corto(){
-    	return ucfirst($this->personas->primer_nombre).' '.ucfirst($this->personas->primer_apellido);
+    	return ucfirst($this->persona->primer_nombre).' '.ucfirst($this->persona->primer_apellido);
     }
 
 
