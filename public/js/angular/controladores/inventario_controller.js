b8d75e8dc968589a95db0d89031e7a6159e30aa3
Israel Lugo <hostelixisrael@gmail.com>
2016-02-05 12:32:26 -0430

    [MOD] se acomodo el controlador de inventario,se agrego la funcion de registrar almacen

--- public/js/angular/controladores/inventario_controller.js
+++ public/js/angular/controladores/inventario_controller.js
@@ -1,9 +1,22 @@
 /// Controlador para inventario
 
-simci.controller('InventarioController', ['$scope','$http','$log','$route', '$routeParams', '$location', 
-  function ($scope, $http, $log , $route, $routeParams, $location){
+simci.controller('InventarioController', [
+  '$scope',
+  '$http',
+  '$log',
+  '$timeout',
+  '$route', 
+  '$routeParams', 
+  '$location',
+  '$compile',
+  'DTOptionsBuilder', 
+  'DTColumnBuilder',
+  'ToolsService',
+  function ($scope, $http, $log ,$timeout, $route, $routeParams, $location, $compile,DTOptionsBuilder, DTColumnBuilder,ToolsService){
     
     $scope.modulo = {};
+
+    $scope.DatosForm = {}; // Objeto para los datos de formulario
 
     $scope.modulo.nombre = "Inventario";
     $scope.modulo.icono = {
@@ -19,6 +32,12 @@ simci.controller('InventarioController', ['$scope','$http','$log','$route', '$ro
         icono: 'user'
       },
       {
+        nombre:"entrada / salida",
+        descripcion: "Esta opcion le permitira dar entrada o salida al los objetos del inventario",
+        url: "#/inventario/ver/todos",
+        icono: 'user'
+      },
+      {
         nombre:"ver inventario",
         descripcion: "Esta opcion le permitira ver los elementos en el inventario, a su vez tambien podra modificar o eliminar dichos objetos",
         url: "#/inventario/ver/todos",
@@ -27,7 +46,7 @@ simci.controller('InventarioController', ['$scope','$http','$log','$route', '$ro
       {
         nombre:"registrar almacen",
         descripcion: "Opcion para crear nuevos almacenes",
-        url: "#/inventario/crear-almacen",
+        url: "#/inventario/registrar-almacen",
         icono: 'user'
       },
       {
@@ -53,6 +72,71 @@ simci.controller('InventarioController', ['$scope','$http','$log','$route', '$ro
     
     $log.info($routeParams);
     $log.info($location);
+
+
+    if($location.$$url == "/inventario/registrar-almacen"){
+      $scope.mostrar_mensaje = false;
+      
+      $scope.registrar_almacen = function(){
+        
+        var formulario = $('#formulario_registrar_almacen');
+        var is_valid_form = formulario.form(reglas_formulario_registrar_almacen).form('is valid');
+
+        if(is_valid_form){
+            
+            //Activamos el loading
+            ToolsService.loading_button('btn-registrar',true);    
+
+            $http({
+              method: 'POST',
+              url: '/api/inventario/registrar-almacen',
+              data: $scope.DatosForm
+            }).then(function(data){
+
+              if(data.data.resultado){
+                
+                $scope.mostrar_mensaje = true;
+                $scope.mensaje_validacion = {
+                  titulo: 'Almacen creado con exito',
+                  icono: 'checkmark',
+                  color: 'green',
+                  mensajes: ['El almacen ha sido registrado en la base de datos.']
+                };
+
+                $timeout(function(){
+                  //Desactivamos el loading
+                  ToolsService.loading_button('btn-registrar',false);    
+                  formulario.form('clear');
+                }, 0, false);
+
+              }
+              else{
+
+                $scope.mostrar_mensaje = true;
+                $scope.mensaje_validacion = {
+                  titulo: 'Hubo un error al guardar el formulario',
+                  icono: 'remove',
+                  color: 'red',
+                  mensajes: data.data.mensajes
+                };
+              }
+
+              //Desactivamos el loading
+              ToolsService.loading_button('btn-registrar',false);
+
+            },function(data_error){
+
+              console.log(data_error);
+
+              //Desactivamos el loading
+              ToolsService.loading_button('btn-registrar',false);
+            });
+            
+        } //If condicional
+      }
+    }///inventario/registrar-almacen
+
+
 
 
     
