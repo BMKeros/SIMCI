bf8a1461fcfea156c46507fcf3e42e0fd8d81c16
Israel Lugo <hostelixisrael@gmail.com>
2016-03-07 13:24:21 -0430

    [MOD] corregida la indexacion de routes.php

--- app/routes.php
+++ app/routes.php
@@ -33,9 +33,9 @@ Route::group(array('prefix' => 'api'), function(){
 	Route::controller('/inventario', 'InventarioController');
 	Route::controller('/catalogo', 'CatalogoController');
 	Route::controller('/laboratorio', 'LaboratorioController');
-  Route::controller('/notificaciones', 'NotificacionesController');
-  Route::controller('/consultas', 'ConsultasController');
-  Route::controller('/proveedores', 'ProveedoresController');
+  	Route::controller('/notificaciones', 'NotificacionesController');
+  	Route::controller('/consultas', 'ConsultasController');
+  	Route::controller('/proveedores', 'ProveedoresController');
 });
 
 Route::controller('/autenticacion', 'AutenticacionController');
