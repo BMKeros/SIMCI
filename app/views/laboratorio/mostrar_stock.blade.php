2576977c76a727055d479c78e3761cf112bb433e
danzt <danztdw@gmail.com>
2016-03-07 20:53:13 -0430

    [MOD] modificado el controlador de laboratorio para anadir la tabla de mostrar stock, [add] se anadio la vista de mostrar stock y, [MOD] modificado el ng-routes en rutas nuevas de mostrar stock, [FIX] reparado algunos detalles en el ng-routes en la parte de stock

---
+++ app/views/laboratorio/mostrar_stock.blade.php
@@ -0,0 +1,8 @@
+<!--Bloque 1 -> Tabla Principal-->
+<div class="ui two column doubling stackable grid container">
+   <div class="ui container centered grid">
+      <div class="column">
+         <table class="ui selectable celled table capitalize" datatable="" dt-options="opciones_tabla_stock" dt-columns="columnas_tabla_stock" dt-instance='tabla_stock' width="100%"></table>
+      </div>
+   </div>
+</div>
