4ada5adfb3e621eccbd343dc18c25ab455e43253
Israel Lugo <hostelixisrael@gmail.com>
2016-03-15 09:53:18 -0430

    [ADD] se agrego un nuevo archivo, de vistas db en el estaran todas las vistas de la db

---
+++ app/database/archivos_sql/vistas_db.sql
@@ -0,0 +1,23 @@
+--Vista para obtener todos los campos de usuario con sus relaciones
+CREATE OR REPLACE VIEW vista_usuarios_full AS 
+	SELECT 
+		usuarios.id  as id_usuario,
+		usuarios.usuario as usuario,
+		usuarios.email as email,
+		usuarios.imagen as imagen,
+		usuarios.activo as activo,
+		usuarios.cod_tipo_usuario as cod_tipo_usuario,
+		tipos_usuario.nombre as tipo_usuario_nombre,
+		tipos_usuario.descripcion as tipo_usuario_descripcion,
+		personas.id as id_persona,
+		personas.primer_nombre as primer_nombre,
+		personas.segundo_nombre  as segundo_nombre,
+		personas.primer_apellido as primer_apellido,
+		personas.segundo_apellido as segundo_apellido,
+		personas.cedula as cedula,
+		personas.fecha_nacimiento as fecha_nacimiento,
+		sexos.descripcion as sexo
+	FROM usuarios
+	INNER JOIN tipos_usuario ON tipos_usuario.codigo = usuarios.cod_tipo_usuario
+	INNER JOIN personas ON personas.usuario_id = usuarios.id
+	INNER JOIN sexos ON personas.sexo_id = sexos.id;
