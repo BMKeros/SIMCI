99393c14cfaaccfdd5b04a66c6f6f22d1fc51812
danzt <danztty@gmail.com>
2016-03-16 19:26:46 -0430

    [MOD] agregado descripcion a los submodulos de instituciones. solo estan de pruebas, falta ver que tal quedaron.

--- public/js/angular/controladores/instituciones_controller.js
+++ public/js/angular/controladores/instituciones_controller.js
@@ -27,32 +27,45 @@ simci.controller('InstitucionesController', [
     $scope.modulo.opciones = [
       {
         nombre:"ministerio de defensa",
-        descripcion: "Esta opcion le redireccionara al portal principal del Ministerio de defensa",
-        url: "http://www.google.com",
+        descripcion: "El Ministerio del Poder Popular para la Defensa es el máximo órgano administrativo en materia de Defensa integral de la Nación, encargado de la formulación, adopción,   seguimiento y evaluación de las políticas, estrategias, planes, programas y proyectos del Sector Defensa.",
+        url: "http://www.mindefensa.gob.ve/",
         icono: 'legal',
         show_in: [TIPO_USER_ROOT, TIPO_USER_ADMIN, TIPO_USER_PROFESOR, TIPO_USER_SUPERVISOR, TIPO_USER_ESTUDIANTE, TIPO_USER_ALMACENISTA]
       },
       {
-        nombre:"DARFA",
-        descripcion: "Esta opcion le redireccionara al portal principal del DARFA",
-        url: "http://www.google.com",
-        icono: 'lab',
-        show_in: [TIPO_USER_ROOT, TIPO_USER_ADMIN, TIPO_USER_PROFESOR, TIPO_USER_SUPERVISOR, TIPO_USER_ESTUDIANTE, TIPO_USER_ALMACENISTA]
-      },
-      {
         nombre:"UPTAG",
-        descripcion: "Esta opcion le redireccionara al portal principal de Universidad Politecnica Tecnologica Alonso Gamero.",
-        url: "http://www.google.com",
+        descripcion: "Institución Universitaria líder, con un perfil continuado de excelencia, egresados de preferencia en el mercado laboral; impulsadores de una sociedad productiva basada en principios de calidad, equidad, solidaridad y compromiso.",
+        url: "http://iutag.org/",
         icono: 'university',
         show_in: [TIPO_USER_ROOT, TIPO_USER_ADMIN, TIPO_USER_PROFESOR, TIPO_USER_SUPERVISOR, TIPO_USER_ESTUDIANTE, TIPO_USER_ALMACENISTA]
       },
       {
         nombre:"bomberos nacionales",
         descripcion: "Esta opcion le redireccionara al portal principal de los bomberos nacionales",
-        url: "http://www.google.com",
+        url: "http://",
         icono: 'fire',
         show_in: [TIPO_USER_ROOT, TIPO_USER_ADMIN, TIPO_USER_PROFESOR, TIPO_USER_SUPERVISOR, TIPO_USER_ESTUDIANTE, TIPO_USER_ALMACENISTA]
       },
+      {
+        nombre:"daex",
+        descripcion: "Organizacion con el fin de recepcionar, almacenar, distribuir y asignar armas, municiones, explosivos, sustancias químicas, radioactivas; y afines, así como controlar la fabricación, el uso, manejo, transporte y comercialización de los mismos.",
+        url: "http://www.daex.mil.ve/",
+        icono: 'fire',
+        show_in: [TIPO_USER_ROOT, TIPO_USER_ADMIN, TIPO_USER_PROFESOR, TIPO_USER_SUPERVISOR, TIPO_USER_ESTUDIANTE, TIPO_USER_ALMACENISTA]
+      },
+      {
+        nombre:"numero de emergencias nacionales",
+        descripcion: "Esta opcion le redireccionara al portal principal de los bomberos nacionales",
+        url: "http://www.ultimasnoticias.com.ve/noticias/ciudad/servicios-publicos/numeros-de-emergencia-en-todo-el-pais.aspx",
+        icono: 'fire',
+        show_in: [TIPO_USER_ROOT, TIPO_USER_ADMIN, TIPO_USER_PROFESOR, TIPO_USER_SUPERVISOR, TIPO_USER_ESTUDIANTE, TIPO_USER_ALMACENISTA]
+      },{
+        nombre:"cicpc",
+        descripcion: "Cuerpo de Investigaciones Científicas, Penales y Criminalísticas. Institución que garantiza la eficiencia en la Investigación del delito, mediante su determinación científica, asegurando el ejercicio de la acción penal que conduzca a una sana administración de justicia.",
+        url: "https://www.cicpc.gob.ve/",
+        icono: 'fire',
+        show_in: [TIPO_USER_ROOT, TIPO_USER_ADMIN, TIPO_USER_PROFESOR, TIPO_USER_SUPERVISOR, TIPO_USER_ESTUDIANTE, TIPO_USER_ALMACENISTA]
+      }
       
 
     ];
