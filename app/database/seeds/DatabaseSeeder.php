7ca355512ebbca282805fc6615a9fe1068250a2f
Israel Lugo <hostelixisrael@gmail.com>
2016-03-18 11:07:26 -0430

    [MOD] se agrego el seeder para las vistas de la db

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -30,6 +30,7 @@ class DatabaseSeeder extends Seeder {
 		$this->call('MunicipiosTableSeeder');
 		$this->call('ParroquiasTableSeeder');
 		$this->call('FuncionesDBSeeder');
+		$this->call('VistasDBSeeder');
 	}
 }
 
