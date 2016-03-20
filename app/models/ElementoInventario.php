6a3ebcbc81d8c8887c95de317b335fbb6b60c788
Israel Lugo <hostelixisrael@gmail.com>
2016-03-20 14:58:24 -0430

    [FIX] reparado el error que daba al momento de crear un elemento en el inventario

--- app/models/ElementoInventario.php
+++ app/models/ElementoInventario.php
@@ -2,6 +2,8 @@
 class ElementoInventario extends Eloquent{
 
 	protected $table = 'inventario';
+	protected $primaryKey = null;
+	public $incrementing = false;
 	protected $fillable = array('cod_dimension', 'cod_subdimension', 'cod_agrupacion', 'cod_subagrupacion', 'numero_orden', 'cod_objeto', 'cantidad_disponible', 'usa_recipientes', 'recipientes_disponible');
 }
 ?>
