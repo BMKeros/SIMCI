140bc4b0ba717b156bc3cf8eaaaa9f42b9a930c5
Israel Lugo <hostelixisrael@gmail.com>
2016-03-20 20:18:20 -0430

    [MOD] modificado los pk del modelo de inventario

--- app/models/Inventario.php
+++ app/models/Inventario.php
@@ -1,8 +1,9 @@
 <?php  
 	class Inventario extends Eloquent{
 		protected $table = 'inventario';
+		protected $primaryKey = null;
+		public $incrementing = false;
 		protected $fillable = array('cod_dimension', 'cod_subdimension', 'cod_agrupacion', 'cod_subagrupacion', 'numero_orden', 'cod_objeto', 'cantidad_disponible', 'usa_recipientes', 'stock', 'recipientes_disponibles');
-		protected $primaryKey = array('cod_dimension', 'cod_subdimension', 'cod_agrupacion', 'cod_objeto');
 		
 	}
 
