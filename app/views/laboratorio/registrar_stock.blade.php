564799093b46d6f703043e785848a8df0a0c932e
ivantelix <ivantelix@gmail.com>
2016-03-21 22:04:19 -0430

    [MOD]agregado select tipo search para la busqueda de elemnto

--- app/views/laboratorio/registrar_stock.blade.php
+++ app/views/laboratorio/registrar_stock.blade.php
@@ -35,16 +35,18 @@
 				<div class="field">
 					<div class="two fields">
 						<div class="seven wide field">
-							<label>Seleccione un elemento</label>
-							<select ng-model="select_objeto" class="ui dropdown">
-								<option value="Encubadora">Encubadora</option>
-								<option value="Pipeta">Pipeta</option>
-								<option value="Campana de Estraccion">Campana de Estraccion</option>
-								<option value="Pinzas">Pinzas</option>
-								<option value="Tubo de ensayo">Tubo de ensayo</option>
-							</select>
-						</div>
+							<div class="field">
+								<label>Seleccione un elemento</label>
 
+								<div class="ui search selection dropdown capitalize buscar_elemento">
+						           	<input type="hidden" ng-model="DatosForm.cod_objeto" name="cod_objeto" ng-update-hidden>
+						           	<div class="text">Buscar elemento</div>
+						           	<i class="dropdown icon"></i>
+						           	<input tabindex="0" class="search" type="text">
+								</div>
+						</div>
+							</div>
+						</div>
 						<div class="field">
 						    <button class="ui icon large inverted green button" id="btn_agregar_items" ng-click="agregar_stock_tabla()"><i class="plus icon" ></i></button>
 
@@ -99,4 +101,13 @@
 
 <script>
 	$('.ui.dropdown').dropdown();
+
+	$('.buscar_elemento').dropdown({
+	apiSettings: {
+	  	method: 'GET',
+	  	dataType: 'JSON',
+	  	url: '/api/inventario/mostrar?type=query&query={query}',
+  	},
+  	saveRemote:false
+});
 </script>
