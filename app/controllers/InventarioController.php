037d147a348a3123a6165854851a74b7dcd1c5f3
Israel Lugo <hostelixisrael@gmail.com>
2016-03-23 17:43:40 -0430

    [MOD] agregada un nuevo case search para busquedas de elementos

--- app/controllers/InventarioController.php
+++ app/controllers/InventarioController.php
@@ -105,6 +105,18 @@
 					}
 				break;
 
+				case 'search':
+					$value_search = Input::get('query');
+
+					$data = DB::table('vista_inventario')
+					->select('cod_objeto', 'nombre_objeto', 'especificaciones_objeto', 'descripcion_objeto','nombre_clase_objeto')
+					->where('nombre_objeto','ILIKE','%'.$value_search.'%')
+					->get();
+
+					$response = array("success"=>true, "results" => $data);
+
+				break;
+
 				default:
 					$response = array();
 				break;
