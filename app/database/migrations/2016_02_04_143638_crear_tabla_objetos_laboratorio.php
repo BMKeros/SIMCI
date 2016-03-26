45fd05bc434f40d9e38911da8af85e2aedf21a17
Israel Lugo <hostelixisrael@gmail.com>
2016-03-26 00:12:00 -0430

    [MOD] agregado el campo cantidad a la migracion de objetos_laboratorio

--- app/database/migrations/2016_02_04_143638_crear_tabla_objetos_laboratorio.php
+++ app/database/migrations/2016_02_04_143638_crear_tabla_objetos_laboratorio.php
@@ -5,30 +5,31 @@ use Illuminate\Database\Migrations\Migration;
 
 class CrearTablaObjetosLaboratorio extends Migration {
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::create('objetos_laboratorio', function($table)
-		{
-			$table->increments('id');
-			$table->string('cod_laboratorio', 4);
-			$table->integer('cod_objeto');
-			$table->nullableTimestamps();
-		});
-	}
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('objetos_laboratorio', function($table)
+        {
+            $table->increments('id');
+            $table->string('cod_laboratorio', 4);
+            $table->integer('cod_objeto');
+            $table->integer('cantidad');
+            $table->nullableTimestamps();
+        });
+    }
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::drop('objetos_laboratorio');
-	}
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('objetos_laboratorio');
+    }
 
 }
