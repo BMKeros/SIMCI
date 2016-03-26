84160204be3642c5789d680d72ade0d53f2cc6c9
Israel Lugo <hostelixisrael@gmail.com>
2016-03-26 00:04:36 -0430

    [MOD] cambios en el gulpfile ahora no se cierra como antes, si no que muestra donde esta el error gracias a la tarea de validar_controladores

--- public/gulpfile.js
+++ public/gulpfile.js
@@ -5,32 +5,34 @@ var uglify = require('gulp-uglify');
 var debug = require('gulp-debug');
 var jsValidate = require('gulp-jsvalidate');
 //var livereload = require('gulp-livereload');
- 
+
 gulp.task('validar_controladores', function () {
-  return gulp.src('./js/angular/controladores/*.js')
-    .pipe(jsValidate());
+    return gulp.src('./js/angular/controladores/*.js')
+        .pipe(jsValidate());
 });
 
-gulp.task('crear_controladores',function(){
-  gulp.src('./js/angular/controladores/*.js')
-  .pipe(debug())
-  .pipe(concat('ng-controladores.js'))
-  .pipe(uglify())
-  .pipe(gulp.dest('./js/angular/'))
+gulp.task('crear_controladores',['validar_controladores'],function(){
+    gulp.src('./js/angular/controladores/*.js')
+        .pipe(debug())
+        .pipe(concat('ng-controladores.js'))
+        .pipe(uglify()).on('error', function (e) {
+            _("Ha ocurrido un error \n %s", e.message);
+        })
+        .pipe(gulp.dest('./js/angular/'))
 });
 
 gulp.task('listen_modificaciones',['crear_controladores'],function(){
-  gulp.watch('./js/angular/controladores/*.js', ['crear_controladores'], function(e){
-    _("archivo %s [%s]",e.type,e.path);
-  })
+    gulp.watch('./js/angular/controladores/*.js', ['crear_controladores'], function(e){
+        _("archivo %s [%s]",e.type,e.path);
+    })
 });
 
 /*gulp.task('listen_autoreload', function() {
-  livereload.listen();
+ livereload.listen();
 
-  gulp.watch('./js/angular/*.js', function(){
-    gulp.src('./js/angular/*.js')
-    .pipe(livereload());
-  });
+ gulp.watch('./js/angular/*.js', function(){
+ gulp.src('./js/angular/*.js')
+ .pipe(livereload());
+ });
 
-});*/
+ });*/
