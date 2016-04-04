29b32868d3a85fc2182daa0658d64dc6032caea3
Israel Lugo <hostelixisrael@gmail.com>
2016-04-04 09:12:47 -0430

    [MOD] se corrigieron algunos cosas en la migracion de datasheet

--- app/database/migrations/2016_03_29_032228_crear_tabla_datasheet.php
+++ app/database/migrations/2016_03_29_032228_crear_tabla_datasheet.php
@@ -12,21 +12,21 @@ class CrearTablaDatasheet extends Migration {
 	 */
 	public function up()
 	{
-		Schema::create('datasheet', function($table)
+        Schema::create('datasheets', function ($table)
 		{
 			$table->increments('id');
-			$table->string('nombre', 25);
+            $table->string('nombre', 40);
 			$table->string('codigo', 15);
-			$table->string('n.cas', 15);
-			$table->string('fabricante', 25);
+            $table->string('numero_cas', 15);
+            $table->string('fabricante', 30);
 			$table->string('formula', 25);
 			$table->float('peso_formula', 5, 2);
-			$table->string('informacion_toxica', 150);
-			$table->string('informacion_radiactiva', 150);
-			$table->string('informacion_fisica_visual', 150);
-			$table->string('informacion_proteccion_personal', 150);
-			$table->string('informacion_almacenamiento', 150);
-			$table->string('informacion_fuego', 150);
+            $table->string('informacion_toxica', 400);
+            $table->string('informacion_radiactiva', 400);
+            $table->string('informacion_fisica_visual', 400);
+            $table->string('informacion_proteccion_personal', 400);
+            $table->string('informacion_almacenamiento', 400);
+            $table->string('informacion_fuego', 400);
 			$table->nullableTimestamps();
 		});
 	}
@@ -38,7 +38,8 @@ class CrearTablaDatasheet extends Migration {
 	 */
 	public function down()
 	{
-		Schema::drop('datasheet');
+        Schema::drop('datasheets');
 	}
 
 }
+
