eb52d51fb5295ab454609924efe356d6362ddd4a
Israel Lugo <hostelixisrael@gmail.com>
2016-04-05 11:59:40 -0430

    [MOD] terminado el validator de exists_in_inventario

--- app/validators/validators.php
+++ app/validators/validators.php
@@ -1,6 +1,20 @@
 <?php
 
-Validator::extend('foo', function($field,$value,$parameters){
-    //return true if field value is foo
-    return $value == 'foo';
+Validator::extend('exists_in_inventario', function($field,$value,$parameters){
+
+    $values = expode('-',$value);
+
+    $tmp_dimension = $values[0];
+    $tmp_subdimension = $values[1];
+    $tmp_agrupacion = $values[2];
+    $tmp_objeto = $values[3];
+
+    $existe = DB::table("inventario")
+        ->where('cod_dimension','=',$tmp_dimension)
+        ->where('cod_subdimension','=',$tmp_subdimension)
+        ->where('cod_agrupacion','=',$tmp_agrupacion)
+        ->where('cod_objeto','=',$tmp_objeto)
+        ->count();
+
+    return ($existe >= 1)?( true ):( false );
 });
