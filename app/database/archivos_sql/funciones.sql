b7d07c9bca8143457e94d72aea5f0d8e7e9f92c8
Israel Lugo <hostelixisrael@gmail.com>
2016-04-08 11:30:01 -0430

    [MOD] se modifico la funcion de mover stock para las nuevas modificaciones de los codigos

--- app/database/archivos_sql/funciones.sql
+++ app/database/archivos_sql/funciones.sql
@@ -1,6 +1,6 @@
 ﻿-- Function: capitalize(text)
 
--- DROP FUNCTION capitalize(text);
+DROP FUNCTION IF EXISTS capitalize(text);
 
 CREATE OR REPLACE FUNCTION capitalize(TEXT)
   RETURNS TEXT AS
@@ -19,7 +19,7 @@ COST 100;
 
 -- Function: permisos_usuario(int)
 
--- DROP FUNCTION permisos_usuario(int);
+DROP FUNCTION IF EXISTS permisos_usuario(int);
 
 --Funcion para obtener todos los permisos de un usuario en especifico
 CREATE OR REPLACE FUNCTION permisos_usuario(INT)
@@ -50,58 +50,101 @@ LANGUAGE plpgsql VOLATILE
 COST 100;
 
 
---Funcion usada para mover los objetos de los laboratorios
---DROP FUNCTION IF EXISTS mover_stock( TEXT, TEXT, INTEGER, INTEGER);
-CREATE OR REPLACE FUNCTION mover_stock_laboratorio(_cod_laboratorio TEXT, _cod_laboratorio_destino TEXT,
-                                                   _cod_objeto      INT, _cantidad_mover INT)
+-- Function: public.mover_stock_laboratorio(text, text, text, text, text, integer, integer)
+
+DROP FUNCTION IF EXISTS public.mover_stock_laboratorio(text, text, text, text, text, integer, integer);
+
+CREATE OR REPLACE FUNCTION public.mover_stock_laboratorio(
+  _cod_laboratorio         TEXT,
+  _cod_laboratorio_destino TEXT,
+  _cod_dimension           TEXT,
+  _cod_subdimension        TEXT,
+  _cod_agrupacion          TEXT,
+  _cod_objeto              INTEGER,
+  _cantidad_mover          INTEGER)
+
   RETURNS BOOLEAN AS
   $BODY$
   DECLARE
     cantidad_actual         INTEGER;
     cantidad_restante       INTEGER;
     cantidad_objeto_destino INTEGER;
+    id_objeto               INTEGER;
   BEGIN
-
+    -- Buscamos la cantidad actual del elemento en el laboratorio origen
     SELECT cantidad
+    INTO cantidad_actual
     FROM objetos_laboratorio
-    WHERE cod_laboratorio = _cod_laboratorio :: TEXT AND cod_objeto = _cod_objeto
-    INTO cantidad_actual;
+    WHERE
+      cod_laboratorio = _cod_laboratorio :: TEXT AND
+      cod_dimension = _cod_dimension AND
+      cod_subdimension = _cod_subdimension AND
+      cod_agrupacion = _cod_agrupacion AND
+      cod_objeto = _cod_objeto;
+
     SELECT cantidad_actual - _cantidad_mover
     INTO cantidad_restante;
+
+    --IF cantidad_restante = 0
+    --THEN
+    --  DELETE FROM objetos_laboratorio WHERE
+    --    cod_laboratorio = _cod_laboratorio :: TEXT AND
+    --    cod_dimension = _cod_dimension AND
+    --    cod_subdimension = _cod_subdimension AND
+    --    cod_agrupacion = _cod_agrupacion AND
+    --    cod_objeto = _cod_objeto;
+    --END IF
 
     IF cantidad_actual <> cantidad_restante AND cantidad_restante >= 0
     THEN
       ---Si cambio la cantidad se actualiza el objeto en la tabla
       UPDATE objetos_laboratorio
       SET cantidad = cantidad_restante
-      WHERE cod_laboratorio = _cod_laboratorio :: TEXT AND cod_objeto = _cod_objeto;
+      WHERE
+        cod_laboratorio = _cod_laboratorio :: TEXT AND
+        cod_dimension = _cod_dimension AND
+        cod_subdimension = _cod_subdimension AND
+        cod_agrupacion = _cod_agrupacion AND
+        cod_objeto = _cod_objeto;
 
       --Verificamos si el objeto que se movera existe en el laboratorio destino
+      SELECT
+        id,
+        cantidad
+      INTO id_objeto, cantidad_objeto_destino
+      FROM objetos_laboratorio
+      WHERE cod_laboratorio = _cod_laboratorio_destino :: TEXT AND
+            cod_dimension = _cod_dimension AND
+            cod_subdimension = _cod_subdimension AND
+            cod_agrupacion = _cod_agrupacion AND
+            cod_objeto = _cod_objeto;
 
-      IF EXISTS(
-          SELECT cantidad
-          FROM objetos_laboratorio
-          WHERE cod_laboratorio = _cod_laboratorio_destino :: TEXT AND cod_objeto = _cod_objeto
-      )
+      -- Si el objeto existe en el laboratorio destino se actualiza
+      IF id_objeto IS NOT NULL
       THEN
-
-        SELECT cantidad
-        INTO cantidad_objeto_destino
-        FROM objetos_laboratorio
-        WHERE cod_laboratorio = _cod_laboratorio_destino :: TEXT AND cod_objeto = _cod_objeto;
 
         --Si existe el objeto en el laboratorio destino se actualiza su cantidad
         UPDATE objetos_laboratorio
         SET cantidad = (cantidad_objeto_destino + _cantidad_mover), updated_at = NOW()
-        WHERE cod_laboratorio = _cod_laboratorio_destino :: TEXT AND cod_objeto = _cod_objeto;
+        WHERE
+          cod_laboratorio = _cod_laboratorio_destino :: TEXT AND
+          cod_dimension = _cod_dimension AND
+          cod_subdimension = _cod_subdimension AND
+          cod_agrupacion = _cod_agrupacion AND
+          cod_objeto = _cod_objeto;
 
         RETURN TRUE; --Retornamos true para hacer saber que se hizo el procedimiento
 
       ELSE
         --Si no existe el objeto en el laboratorio destino se crear con la cantidad
-        INSERT INTO public.objetos_laboratorio (
-          cod_laboratorio, cod_objeto, cantidad, created_at, updated_at)
-        VALUES (_cod_laboratorio_destino, _cod_objeto, _cantidad_mover, NOW(), NOW());
+        INSERT INTO objetos_laboratorio (
+          cod_laboratorio, cod_dimension, cod_subdimension, cod_agrupacion,
+          cod_objeto, cantidad, created_at, updated_at)
+        VALUES (_cod_laboratorio_destino,
+                _cod_dimension,
+                _cod_subdimension,
+                _cod_agrupacion,
+                _cod_objeto, _cantidad_mover, NOW(), NOW());
 
         RETURN TRUE; --Retornamos true para hacer saber que se hizo el procedimiento
 
@@ -112,13 +155,13 @@ CREATE OR REPLACE FUNCTION mover_stock_laboratorio(_cod_laboratorio TEXT, _cod_l
   END;
   $BODY$
 LANGUAGE plpgsql VOLATILE
+COST 100;
 
 
 
--- DROP FUNCTION public.agregar_stock_laboratorio(text, text, text,integer, text ,integer);
 -- Function: public.agregar_stock_laboratorio(text, text, text,integer, text ,integer);
 
-DROP FUNCTION public.agregar_stock_laboratorio( TEXT, TEXT, TEXT, INTEGER, TEXT, INTEGER );
+DROP FUNCTION IF EXISTS public.agregar_stock_laboratorio( TEXT, TEXT, TEXT, INTEGER, TEXT, INTEGER );
 
 CREATE OR REPLACE FUNCTION public.agregar_stock_laboratorio(
   _cod_dimension    TEXT,
@@ -131,13 +174,14 @@ CREATE OR REPLACE FUNCTION public.agregar_stock_laboratorio(
   RETURNS BOOLEAN AS
   $BODY$
   DECLARE
-    id_stock_laboratorio INTEGER;
-    cantidad_existente   INTEGER;
+    id_stock_laboratorio           INTEGER;
+    cantidad_existente             INTEGER;
     cantidad_disponible_inventario INTEGER;
   BEGIN
     --Consultamos al inventario para ver si la cantidad que se esta
     --agregando no es mayor a la existente
-    SELECT cantidad_disponible::INTEGER INTO cantidad_disponible_inventario
+    SELECT cantidad_disponible :: INTEGER
+    INTO cantidad_disponible_inventario
     FROM inventario
     WHERE
       cod_dimension = _cod_dimension AND
