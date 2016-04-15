3a6dd84d43050fa90a15648d4239fad33521d68a
Israel Lugo <hostelixisrael@gmail.com>
2016-04-15 10:00:12 -0430

    [MOD] se agrego el created_at y updated_at a la vista de mostrar proveedores

--- app/views/proveedor/mostrar_proveedores.blade.php
+++ app/views/proveedor/mostrar_proveedores.blade.php
@@ -90,11 +90,11 @@
             <tr>
                 <td colspan="4">
                     <b>Creado:</b><br>
-                    <p>2016/04/28</p>
+                    <p><% data_proveedor.created_at | formato_timestamps %></p>
                 </td>
                 <td colspan="4">
                     <b>Actualizado:</b><br>
-                    <p>2016/06/28</p>
+                    <p><% data_proveedor.updated_at | formato_timestamps %></p>
                 </td>
             </tr>
 
