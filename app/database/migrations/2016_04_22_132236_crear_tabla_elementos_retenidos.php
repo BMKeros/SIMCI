8ba7c52af3a94744fbcb5cf41f76f7d175eb3a73
ivantelix <ivantelix@gmail.com>
2016-04-27 13:38:27 -0430

    [MOD]agregado los cuatro codigos a la tabla elementos retenidos

--- app/database/migrations/2016_04_22_132236_crear_tabla_elementos_retenidos.php
+++ app/database/migrations/2016_04_22_132236_crear_tabla_elementos_retenidos.php
@@ -19,6 +19,13 @@ class CrearTablaElementosRetenidos extends Migration {
 			$table->decimal('cantidad_solicitada');
 			$table->integer('cod_referencia');//codigo que referenciara a Elem-Ret;Pedido
 			$table->integer('cod_tipo_movimiento');
+			$table->integer('numero_orden');
+
+			//estos son los primary
+			$table->string('cod_dimension', 4);
+			$table->string('cod_subdimension', 3);
+			$table->string('cod_agrupacion', 3);
+			$table->integer('cod_objeto');
 
 			$table->nullableTimestamps();
 		});
