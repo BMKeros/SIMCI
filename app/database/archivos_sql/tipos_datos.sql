10e307b7f0556f48c39c5ddad91a6a92c316cee1
Israel Lugo <hostelixisrael@gmail.com>
2016-04-28 12:14:38 -0430

    [ADD] agregado nuevos tipos de datos

---
+++ app/database/archivos_sql/tipos_datos.sql
@@ -0,0 +1,8 @@
+CREATE TYPE TypeElemento AS (
+  cod_dimension    TEXT,
+  cod_subdimension TEXT,
+  cod_agrupacion   TEXT,
+  cod_objeto       INTEGER,
+  numero_orden     INTEGER,
+  cantidad_disponible NUMERIC
+);
