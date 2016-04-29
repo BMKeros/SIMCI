804cb8b1b3187f99bf9f112dfbf6638546c0bc9e
Israel Lugo <hostelixisrael@gmail.com>
2016-04-29 09:31:29 -0430

    [MOD] se modifico la migracion de elementos retenidos se cambio el orden en como estabas los campos

--- app/database/migrations/2016_04_22_132236_crear_tabla_elementos_retenidos.php
+++ app/database/migrations/2016_04_22_132236_crear_tabla_elementos_retenidos.php
@@ -3,42 +3,40 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CrearTablaElementosRetenidos extends Migration {
+class CrearTablaElementosRetenidos extends Migration
+{
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::create('elementos_retenidos', function(Blueprint $table)
-		{
-			$table->increments('id');
-			$table->decimal('cantidad_existente');
-			$table->decimal('cantidad_solicitada');
-			$table->integer('cod_referencia');//codigo que referenciara a Elem-Ret;Pedido
-			$table->integer('cod_tipo_movimiento');
-			$table->integer('numero_orden');
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('elementos_retenidos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('cod_dimension', 4);
+            $table->string('cod_subdimension', 3);
+            $table->string('cod_agrupacion', 3);
+            $table->integer('cod_objeto');
+            $table->integer('numero_orden');
+            $table->decimal('cantidad_existente');
+            $table->decimal('cantidad_solicitada');
+            $table->integer('cod_referencia');//codigo que referenciara a Elem-Ret;Pedido
+            $table->integer('cod_tipo_movimiento');
 
-			//estos son los primary
-			$table->string('cod_dimension', 4);
-			$table->string('cod_subdimension', 3);
-			$table->string('cod_agrupacion', 3);
-			$table->integer('cod_objeto');
+            $table->nullableTimestamps();
+        });
+    }
 
-			$table->nullableTimestamps();
-		});
-	}
-
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::drop('elementos_retenidos');
-	}
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('elementos_retenidos');
+    }
 
 }
