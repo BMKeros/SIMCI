6de8d423b8c7a8bcf1c6afa00d1ab44377c9c986
ivantelix <ivantelix@gmail.com>
2016-05-06 12:47:53 -0400

    [MOD]creado tabla para los elementos_restringidos

---
+++ app/database/migrations/2016_05_06_162157_crear_tabla_elementos_restringidos.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CrearTablaElementosRestringidos extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('elementos_restringidos', function(Blueprint $table)
+		{
+			$table->increments('id');
+            $table->string('cod_dimension', 4);
+			$table->string('cod_subdimension', 3);
+			$table->string('cod_agrupacion', 3);
+			$table->integer('cod_objeto');           
+
+			$table->nullableTimestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('elementos_restringidos');
+	}
+
+}
