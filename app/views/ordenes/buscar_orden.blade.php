47131a4ae24554734d02bc6670f9e50c5eb2ea73
Israel Lugo <hostelixisrael@gmail.com>
2016-05-08 00:56:47 -0400

    [MOD] agregada la directiva qr-scanner a buscar ordene

--- app/views/ordenes/buscar_orden.blade.php
+++ app/views/ordenes/buscar_orden.blade.php
@@ -22,9 +22,31 @@
 
             <form id="formulario_buscar_orden">
 
+                <div class="ui form">
+                    <div class="inline fields">
+                        <label>Como desea buscar la orden?</label>
+
+                        <div class="ui selection dropdown">
+                            <input name="gender" type="hidden">
+                            <i class="dropdown icon"></i>
+
+                            <div class="default text">Seleccione una opcion</div>
+                            <div class="menu">
+                                <div class="item" data-value="1">Codigo normal</div>
+                                <div class="item" data-value="0">Codigo QR</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <qr-scanner ng-success="onSuccess(data)" width="400" height="300"></qr-scanner>
 
             </form>
         </div>
 
     </div>
 </div>
+
+<script>
+    $('.ui.dropdown').dropdown();
+</script>
