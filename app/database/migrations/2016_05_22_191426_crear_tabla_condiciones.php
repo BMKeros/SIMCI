3c5888e024d17b6f0f3f73a6cff32c393951abf1
ivantelix <ivantelix@gmail.com>
2016-05-22 15:54:41 -0400

    [MOD]creado nueva tabla condiciones y cambiado el index para que funcione con la tabla ordenes y la index

---
+++ app/database/migrations/2016_05_22_191426_crear_tabla_condiciones.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CrearTablaCondiciones extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('condiciones', function(Blueprint $table)
+		{
+			$table->string('codigo', 3);
+			$table->primary('codigo');
+			$table->string('nombre', 50);
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('condiciones');
+	}
+
+}
