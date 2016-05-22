9444473427518d5439d0fe7ff5c81780c874a72a
Israel Lugo <hostelixisrael@gmail.com>
2016-05-21 22:24:46 -0400

    [FIX] reparado error del commit anterior

--- app/database/seeds/EstadosOrdenesTableSeeder.php
+++ app/database/seeds/EstadosOrdenesTableSeeder.php
@@ -9,7 +9,7 @@ class EstadosOrdenesTableSeeder extends Seeder
         DB::table('estados_ordenes')->delete();
 
         $campos = array(
-            array('codigo' => 'E01', 'nombre' => 'Activo'),
+            array('codigo' => 'E01', 'nombre' => 'Activa'),
             array('codigo' => 'E02', 'nombre' => 'Pendiente'),
             array('codigo' => 'E03', 'nombre' => 'Cancelada'),
             array('codigo' => 'E04', 'nombre' => 'Completada'),
