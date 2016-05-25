a1f1f4f8da7674097f5b5a33734ee15883fb2245
Israel Lugo <hostelixisrael@gmail.com>
2016-05-24 21:45:51 -0400

    [MOD] ajustes en la consulta de estadisticas

--- app/routes.php
+++ app/routes.php
@@ -31,10 +31,10 @@ Route::get('/estadisticas', function () {
         (SELECT count(*) FROM usuarios)::INTEGER AS total_usuarios,
         (SELECT count(*) FROM catalogo_objetos)::INTEGER AS total_objetos,
         (SELECT count(*) FROM inventario)::INTEGER AS total_elementos,
-        (SELECT count(*) FROM ordenes WHERE status = 'E01')::INTEGER AS total_ordenes_activas,
-        (SELECT count(*) FROM ordenes WHERE status = 'E02')::INTEGER AS total_ordenes_pendientes;
+        (SELECT count(*) FROM ordenes WHERE status = '" . ORDEN_ACTIVA . "' )::INTEGER AS total_ordenes_activas,
+        (SELECT count(*) FROM ordenes WHERE status = '" . ORDEN_PENDIENTE . "' )::INTEGER AS total_ordenes_pendientes;
     "));
-
+    
 
     return Response::json(['indicadores' => $datos[0]]);
 });
