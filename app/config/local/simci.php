f62ca6ac8338305972341e5912d0a3e9829834b7
Israel Lugo <hostelixisrael@gmail.com>
2016-05-24 21:03:55 -0400

    [ADD] agregada la configuracion local para mailgun nuevo archivo simci.php

---
+++ app/config/local/simci.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'MAILGUN' => [
+        'USER' => 'postmaster@sandbox69403da1cf164af0b057d1c6cfa5ac78.mailgun.org',
+        'PASS' => '06dbaba2b37c559103ac746c886661cf',
+        'EMAIL' => 'bmkeros@gmail.com',
+        'NAME' => "BMKero's",
+        'API' => [
+            'URL' => 'https://api.mailgun.net/v3/sandbox69403da1cf164af0b057d1c6cfa5ac78.mailgun.org',
+            'KEY' => 'key-c17e3edcf3e15a522e2517c778b7e590'
+        ]
+    ]
+];
+
