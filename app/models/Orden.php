a32dc2ab0ef0b1f7732ce5178da32e12590a2830
Israel Lugo <hostelixisrael@gmail.com>
2016-05-30 23:41:47 -0400

    [MOD] modificaciones al modelo de orden , tambien se agrego una funcion para obtener los datos del reponsable como el nombre completo y el email para enviar el correo

--- app/models/Orden.php
+++ app/models/Orden.php
@@ -1,6 +1,21 @@
 <?php  
 	class Orden extends Eloquent{
 		protected $table = 'ordenes';
-		protected $fillable = array('id', 'codigo', 'usuario_id', 'fecha', 'hora', 'cod_laboratorio', 'observaciones', 'status');
+        protected $fillable = array('id', 'codigo', 'responsable', 'solicitante', 'fecha_actividad', 'fecha', 'hora', 'cod_laboratorio', 'observaciones', 'status');
+
+
+        static public function get_datos_responsable($codigo_orden)
+        {
+            $consulta = DB::table('ordenes')
+                ->select(
+                    RAW('formato_nombre_completo(personas.primer_nombre, personas.primer_apellido) as nombre_completo'),
+                    'usuarios.email')
+                ->join('usuarios', 'usuarios.id', '=', 'ordenes.responsable')
+                ->join('personas', 'personas.usuario_id', '=', 'usuarios.id')
+                ->where('codigo', $codigo_orden)
+                ->first();
+
+            return $consulta;
+        }
 	}
 ?>
