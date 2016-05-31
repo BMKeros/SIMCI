133cbb363521664d3638388e4215ce0413333ca2
Israel Lugo <hostelixisrael@gmail.com>
2016-05-30 22:57:45 -0400

    [ADD] agregada la vista para el email de cancelado

---
+++ app/views/emails/plantilla_cancelar_orden.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+
+<html>
+
+<head lang="es">
+    <title>Orden Cancelada</title>
+</head>
+
+<body>
+<h2>Ordene Cancelada - {{ $cod_orden }}</h2>
+
+<p>
+    <b>Su orden fue cancelada por el siguiente motivo: </b>
+    {{ $razon_cancelar }}
+</p>
+
+<br>
+
+<h3>Aqui va la tabla con todos sus pedidos</h3>
+</body>
+
+</html>
