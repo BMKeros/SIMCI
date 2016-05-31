71e93e02019db6b8710839f59b4a1eb5feef4e51
Israel Lugo <hostelixisrael@gmail.com>
2016-05-31 00:14:11 -0400

    [MOD] modificaciones en la plantilla de mail para cancelar orden

--- app/views/emails/plantilla_cancelar_orden.blade.php
+++ app/views/emails/plantilla_cancelar_orden.blade.php
@@ -4,19 +4,42 @@
 
 <head lang="es">
     <title>Orden Cancelada</title>
+
+    <style>
+        #razon_cancelar {
+            font-size: 18px;
+        }
+    </style>
 </head>
 
 <body>
-<h2>Ordene Cancelada - {{ $cod_orden }}</h2>
+<h2>Ordene Cancelada</h2>
+
+<h2>Codigo de la orden: {{ $cod_orden }}</h2>
 
 <p>
     <b>Su orden fue cancelada por el siguiente motivo: </b>
-    {{ $razon_cancelar }}
+    <br>
+    <site id="razon_cancelar">
+        {{ $razon_cancelar }}
+    </site>
 </p>
 
 <br>
 
 <h3>Aqui va la tabla con todos sus pedidos</h3>
+
+
+<hr>
+
+<footer>
+    <p align="center">Plantilla generada por SIMCI v1.0</p>
+
+    <p align="center">
+        <b>SISTEMA INTEGRADO PARA EL MANEJO Y CONTROL DE INVENTARIOS</b>
+    </p>
+
+</footer>
 </body>
 
 </html>
