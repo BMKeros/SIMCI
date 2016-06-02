b31459d4197d5b904a14fcee7355cbae8c4caeb6
Israel Lugo <hostelixisrael@gmail.com>
2016-06-02 00:12:26 -0400

    [ADD] agregada nueva migracion para la tabla de correos, sujeta a cambios

---
+++ app/database/migrations/2016_06_02_040143_crear_tabla_correos.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CrearTablaCorreos extends Migration
+{
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('correos', function ($table) {
+            $table->increments('id');
+            $table->integer('emisor');
+            $table->integer('destinatario');
+            $table->string('asunto', 80);
+            $table->integer('archivo')->nullable();
+            $table->string('descripcion', 250);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        drop_cascade('correos');
+    }
+
+}
