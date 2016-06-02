d75617c43adf43071c6862633421c71fe28b3934
Israel Lugo <hostelixisrael@gmail.com>
2016-06-02 01:23:05 -0400

    [MOD] agregada funcion para enviar correo en el controlador de correos, aun no esta completa

--- app/controllers/CorreosController.php
+++ app/controllers/CorreosController.php
@@ -2,5 +2,22 @@
 
 class CorreosController extends Controller
 {
+    public function postEnviarCorreo()
+    {
+        $emisor = Auth::user()->id;
+        $asunto = Input::get('asunto');
+        $destinatario = Input::get('destinatario');
+        $descripcion = Input::get('descripcion');
+        $archivo = Input::file('archivo');
 
+        if (Input::hasFile('archivo')) {
+
+            $nuevo_archivo = new Archivo();
+            $nuevo_archivo->nombre_original = $archivo->getClientOriginalName();
+            $nuevo_archivo->nombre_generado = generar_nombre_archivo();
+            $nuevo_archivo->extension = get_extension_archivo($archivo->getClientOriginalName());
+            $nuevo_archivo->ubicacion = '';
+
+        }
+    }
 }
