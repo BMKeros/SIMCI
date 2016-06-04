4c70aedf0372ea2e726dfe3a30b36db4275a4839
Israel Lugo <hostelixisrael@gmail.com>
2016-06-03 23:02:13 -0400

    [ADD] agregada nueva tabla de correo_destinatoarios

---
+++ app/database/migrations/2016_06_04_025810_crear_tabla_correo_destinatarios.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CrearTablaCorreoDestinatarios extends Migration
+{
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('correo_destinatarios', function ($table) {
+            $table->increments('id');
+            $table->integer('correo_id');
+            $table->integet('destinatario');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        drop_cascade('correo_destinatarios');
+    }
+
+}
