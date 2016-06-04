123fd9cc099880e79dcbfcfbe9faa0fe0df54986
Israel Lugo <hostelixisrael@gmail.com>
2016-06-03 23:44:53 -0400

    [MOD] se agrego la funcion para obtener el nombre completo con extension ddel archivo

--- app/models/Archivo.php
+++ app/models/Archivo.php
@@ -5,4 +5,9 @@ class Archivo extends Eloquent
     protected $table = 'archivos';
     protected $fillable = array('nombre_original', 'nombre_generado', 'path', 'extension');
 
+    public function get_full_name()
+    {
+        return $this->nombre_generado . '.' . $this->extension;
+    }
+
 }
