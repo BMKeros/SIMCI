f8cf80d73eecea697cd0fbaee4e5f7b7eeaf268c
Israel Lugo <hostelixisrael@gmail.com>
2016-11-05 14:08:00 -0400

    [MOD] agregada la funcion get_datos_solicitante al modelo de Orden

--- app/models/Orden.php
+++ app/models/Orden.php
@@ -17,5 +17,19 @@
 
             return $consulta;
         }
+
+        static public function get_datos_solicitante($codigo_orden)
+        {
+            $consulta = DB::table('ordenes')
+                ->select(
+                    RAW('formato_nombre_completo(personas.primer_nombre, personas.primer_apellido) as nombre_completo'),
+                    'usuarios.email')
+                ->join('usuarios', 'usuarios.id', '=', 'ordenes.solicitante')
+                ->join('personas', 'personas.usuario_id', '=', 'usuarios.id')
+                ->where('codigo', $codigo_orden)
+                ->first();
+
+            return $consulta;
+        }
 	}
 ?>
