9a038fa6be48bce1029b4576df92f430b386320c
Israel Lugo <hostelixisrael@gmail.com>
2016-11-05 20:46:44 -0400

    [MOD]agregada la funcion de get_nombre para el modelo de laboratorio

--- app/models/Laboratorio.php
+++ app/models/Laboratorio.php
@@ -4,5 +4,16 @@ class Laboratorio extends Eloquent{
 	protected $table = 'laboratorios';
 	protected $fillable = array('codigo', 'nombre', 'descripcion');
 	protected $primaryKey = 'codigo';
+
+	static public function get_nombre($cod_laboratorio)
+	{
+
+		$consulta = DB::table('laboratorios')
+			->select(RAW("UPPER(laboratorios.nombre) as resultado"))
+			->where('laboratorios.codigo', '=', $cod_laboratorio)
+			->first();
+
+		return $consulta->resultado;
+	}
 }
 ?>
