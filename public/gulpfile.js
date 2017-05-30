371b1741466afab2c2a94e62f1a298e1209df1b7
Israel Lugo <hostelixisrael@gmail.com>
2017-05-29 21:20:14 -0400

    [MOD] agregadas las nuevas rutinas de gulp para minificar la app angular

--- public/gulpfile.js
+++ public/gulpfile.js
@@ -4,6 +4,8 @@ var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var debug = require('gulp-debug');
 var jsValidate = require('gulp-jsvalidate');
+var ngAnnotate = require('gulp-ng-annotate');
+var sourcemaps = require('gulp-sourcemaps');
 
 
 gulp.task('validar_javascript', function () {
@@ -11,20 +13,24 @@ gulp.task('validar_javascript', function () {
         .pipe(jsValidate());
 });
 
-gulp.task('compilar_archivos_angular',['validar_javascript'],function(){
+gulp.task('compilar_archivos_angular', ['validar_javascript'], function () {
     //'!./js/angular/**/ng-*.js'
     gulp.src(['./js/angular/**/*.js'])
         .pipe(debug())
+        .pipe(sourcemaps.init())
         .pipe(concat('ng-app.min.js'))
-        .pipe(uglify()).on('error', function (e) {
+        .pipe(ngAnnotate())
+        .pipe(uglify()).on('error',
+        function (e) {
             _("Ha ocurrido un error \n %s", e.message);
         })
+        .pipe(sourcemaps.write())
         .pipe(gulp.dest('./js/'))
 });
 
 
-gulp.task('listen_modificaciones',['compilar_archivos_angular'],function(){
-    gulp.watch('./js/angular/*.js', ['compilar_archivos_angular'], function(e){
-        _("archivo %s [%s]",e.type,e.path);
+gulp.task('listen_modificaciones', ['compilar_archivos_angular'], function () {
+    gulp.watch('./js/angular/*.js', ['compilar_archivos_angular'], function (e) {
+        _("archivo %s [%s]", e.type, e.path);
     })
 });
