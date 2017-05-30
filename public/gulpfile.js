b5501fb63fddb06d07705a58966b69cf1b64ee18
Israel Lugo <hostelixisrael@gmail.com>
2017-05-29 20:07:33 -0400

    [MOD] modificaciones en los script del gulp file

--- public/gulpfile.js
+++ public/gulpfile.js
@@ -4,35 +4,27 @@ var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var debug = require('gulp-debug');
 var jsValidate = require('gulp-jsvalidate');
-//var livereload = require('gulp-livereload');
 
-gulp.task('validar_controladores', function () {
-    return gulp.src('./js/angular/controladores/*.js')
+
+gulp.task('validar_javascript', function () {
+    return gulp.src('./js/*.js')
         .pipe(jsValidate());
 });
 
-gulp.task('crear_controladores',['validar_controladores'],function(){
-    gulp.src('./js/angular/controladores/*.js')
+gulp.task('compilar_archivos_angular',['validar_javascript'],function(){
+    //'!./js/angular/**/ng-*.js'
+    gulp.src(['./js/angular/**/*.js'])
         .pipe(debug())
-        .pipe(concat('ng-controladores.js'))
+        .pipe(concat('ng-app.min.js'))
         .pipe(uglify()).on('error', function (e) {
             _("Ha ocurrido un error \n %s", e.message);
         })
-        .pipe(gulp.dest('./js/angular/'))
+        .pipe(gulp.dest('./js/'))
 });
 
-gulp.task('listen_modificaciones',['crear_controladores'],function(){
-    gulp.watch('./js/angular/controladores/*.js', ['crear_controladores'], function(e){
+
+gulp.task('listen_modificaciones',['compilar_archivos_angular'],function(){
+    gulp.watch('./js/angular/*.js', ['compilar_archivos_angular'], function(e){
         _("archivo %s [%s]",e.type,e.path);
     })
 });
-
-/*gulp.task('listen_autoreload', function() {
- livereload.listen();
-
- gulp.watch('./js/angular/*.js', function(){
- gulp.src('./js/angular/*.js')
- .pipe(livereload());
- });
-
- });*/
