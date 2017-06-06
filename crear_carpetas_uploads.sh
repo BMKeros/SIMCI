0e0c3db4daefa55c20fc9de036a46edd2548ce67
Israel Lugo <hostelixisrael@gmail.com>
2017-06-06 13:05:09 -0400

    [MOD] modificaciones en el archivo de crear carpetas necesarias

--- crear_carpetas_uploads.sh
+++ crear_carpetas_uploads.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+#Carpeta para los archivos de usuario
+mkdir -p public/uploads/imagenes/
+chown -R www-data:www-data public/uploads/imagenes/
+
 #Carpeta para los archivos envidos por correo de la app
-mkdir public/uploads/archivos/correo
+mkdir -p public/uploads/archivos/correo
 chown -R www-data:www-data public/uploads/archivos/correo/
