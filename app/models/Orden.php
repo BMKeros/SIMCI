624439817bf98c326aa3e296183a10b81ced9d7c
Israel Lugo <hostelixisrael@gmail.com>
2017-06-12 16:05:07 -0400

    [MOD] se agrego la relacion a nivel de orm con el modelo de orden

--- app/models/Orden.php
+++ app/models/Orden.php
@@ -1,57 +1,66 @@
-<?php  
-	class Orden extends Eloquent{
-		protected $table = 'ordenes';
-        protected $fillable = array('id', 'codigo', 'responsable', 'solicitante', 'fecha_actividad', 'fecha', 'hora', 'cod_laboratorio', 'observaciones', 'status');
+<?php
 
-        public function get_datos_responsable()
-        {
-            $consulta = DB::table('ordenes')
-                ->select(
-                    RAW('formato_nombre_completo(personas.primer_nombre, personas.primer_apellido) as nombre_completo'),
-                    'usuarios.email')
-                ->join('usuarios', 'usuarios.id', '=', 'ordenes.responsable')
-                ->join('personas', 'personas.usuario_id', '=', 'usuarios.id')
-                ->where('codigo', $this->codigo)
-                ->first();
+class Orden extends Eloquent
+{
+    protected $table = 'ordenes';
+    protected $fillable = array('id', 'codigo', 'responsable', 'solicitante', 'fecha_actividad', 'fecha', 'hora', 'cod_laboratorio', 'observaciones', 'status');
 
-            return $consulta->nombre_completo;
-        }
 
-        public function get_datos_solicitante()
-        {
-            $consulta = DB::table('ordenes')
-                ->select(
-                    RAW('formato_nombre_completo(personas.primer_nombre, personas.primer_apellido) as nombre_completo'),
-                    'usuarios.email')
-                ->join('usuarios', 'usuarios.id', '=', 'ordenes.solicitante')
-                ->join('personas', 'personas.usuario_id', '=', 'usuarios.id')
-                ->where('codigo', $this->codigo)
-                ->first();
+    public function pedidos()
+    {
+        return $this->hasMany('Pedido', 'cod_orden', 'codigo');
+    }
 
-            return $consulta->nombre_completo;
-        }
+    public function get_datos_responsable()
+    {
+        $consulta = DB::table('ordenes')
+            ->select(
+                RAW('formato_nombre_completo(personas.primer_nombre, personas.primer_apellido) as nombre_completo'),
+                'usuarios.email')
+            ->join('usuarios', 'usuarios.id', '=', 'ordenes.responsable')
+            ->join('personas', 'personas.usuario_id', '=', 'usuarios.id')
+            ->where('codigo', $this->codigo)
+            ->first();
 
-        public function get_estado_orden()
-        {
-            $consulta = DB::table('ordenes')
-                ->select(
-                    RAW('UPPER(condiciones.nombre) as nombre'))
-                ->join('condiciones', 'condiciones.codigo', '=', 'ordenes.status')
-                ->where('ordenes.codigo', '=', $this->codigo)
-                ->first();
+        return $consulta->nombre_completo;
+    }
 
-            return $consulta->nombre;
-        }
+    public function get_datos_solicitante()
+    {
+        $consulta = DB::table('ordenes')
+            ->select(
+                RAW('formato_nombre_completo(personas.primer_nombre, personas.primer_apellido) as nombre_completo'),
+                'usuarios.email')
+            ->join('usuarios', 'usuarios.id', '=', 'ordenes.solicitante')
+            ->join('personas', 'personas.usuario_id', '=', 'usuarios.id')
+            ->where('codigo', $this->codigo)
+            ->first();
 
-        public function get_nombre_laboratorio()
-        {
-            $consulta = DB::table('laboratorios')
-                ->select(RAW("UPPER(laboratorios.nombre) as nombre_laboratorio"))
-                ->where('laboratorios.codigo', '=', $this->cod_laboratorio)
-                ->first();
+        return $consulta->nombre_completo;
+    }
 
-            return $consulta->nombre_laboratorio;
-        }
+    public function get_estado_orden()
+    {
+        $consulta = DB::table('ordenes')
+            ->select(
+                RAW('UPPER(condiciones.nombre) as nombre'))
+            ->join('condiciones', 'condiciones.codigo', '=', 'ordenes.status')
+            ->where('ordenes.codigo', '=', $this->codigo)
+            ->first();
 
-	}
+        return $consulta->nombre;
+    }
+
+    public function get_nombre_laboratorio()
+    {
+        $consulta = DB::table('laboratorios')
+            ->select(RAW("UPPER(laboratorios.nombre) as nombre_laboratorio"))
+            ->where('laboratorios.codigo', '=', $this->cod_laboratorio)
+            ->first();
+
+        return $consulta->nombre_laboratorio;
+    }
+
+}
+
 ?>
