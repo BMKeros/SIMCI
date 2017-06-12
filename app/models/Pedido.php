92b49a059254b72f6eaf8d1dbcf8144a87f197e1
Israel Lugo <hostelixisrael@gmail.com>
2017-06-12 16:07:40 -0400

    [MOD] se agrego la relacion a nivel de orm con el modelo de pedido con orden

--- app/models/Pedido.php
+++ app/models/Pedido.php
@@ -1,15 +1,24 @@
-<?php  
-	class Pedido extends Eloquent{
-		protected $table = "pedidos";
-		protected $fillable = array('cod_dimension', 
-									'cod_subdimension', 
-									'cod_agrupacion', 
-									'cod_objeto',
-									'numero_orden',
-									'cantidad_disponible',
-									'cantidad_solicitada',
-									'cod_referencia',
-									'cod_tipo_movimiento'
-								);
-	}
+<?php
+
+class Pedido extends Eloquent
+{
+    protected $table = "pedidos";
+    protected $fillable = array(
+        'cod_dimension',
+        'cod_subdimension',
+        'cod_agrupacion',
+        'cod_objeto',
+        'numero_orden',
+        'cantidad_disponible',
+        'cantidad_solicitada',
+        'cod_referencia',
+        'cod_tipo_movimiento'
+    );
+
+    public function orden()
+    {
+        return $this->belongsTo('Orden', 'cod_orden', 'codigo');
+    }
+}
+
 ?>
