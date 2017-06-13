4d57018ba2b7507a11ce01b88811e77d8948d55e
Israel Lugo <hostelixisrael@gmail.com>
2017-06-13 11:02:51 -0400

    [MOD] agregado la funcion nombre completo al modelo de persona

--- app/models/Persona.php
+++ app/models/Persona.php
@@ -1,20 +1,24 @@
-<?php  
-class Persona Extends Eloquent{
-	protected $table = 'personas';
+<?php
 
-	protected $fillable = array('primer_nombre', 'segundo_nombre', 'primer_apellido', 'segundo_apellido', 'cedula', 'usuario_id','sexo_id', 'fecha_nacimiento');
+class Persona Extends Eloquent
+{
+    protected $table = 'personas';
 
-	protected $visible =  array('id','primer_nombre', 'segundo_nombre', 'primer_apellido', 'segundo_apellido', 'cedula', 'usuario_id','sexo_id', 'fecha_nacimiento','data_sexo');
+    protected $fillable = array('primer_nombre', 'segundo_nombre', 'primer_apellido', 'segundo_apellido', 'cedula', 'usuario_id', 'sexo_id', 'fecha_nacimiento');
 
-	public function usuario(){
-		return $this->belongsTo('Usuario');
-	}
+    protected $visible = array('id', 'primer_nombre', 'segundo_nombre', 'primer_apellido', 'segundo_apellido', 'cedula', 'usuario_id', 'sexo_id', 'fecha_nacimiento', 'data_sexo');
 
-	public function sexo(){
-		return $this->belongsTo('Sexo');
-	}
+    public function usuario()
+    {
+        return $this->belongsTo('Usuario');
+    }
 
-	public function getDataSexoAttribute()
+    public function sexo()
+    {
+        return $this->belongsTo('Sexo');
+    }
+
+    public function getDataSexoAttribute()
     {
         return $this->sexo->toArray();
     }
@@ -22,34 +26,43 @@ class Persona Extends Eloquent{
     protected $appends = ['data_sexo'];
 
 
-	public function setPrimerNombreAttibute($value){
-		if (!empty($value)) {
-			$this->attributes['primer_nombre']=strtolower($value);
-		}
-	}
+    public function setPrimerNombreAttibute($value)
+    {
+        if (!empty($value)) {
+            $this->attributes['primer_nombre'] = strtolower($value);
+        }
+    }
 
 
-	public function setSegundoNombreAttibute($value){
-		if (!empty($value)) {
-			$this->attributes['segundo_nombre']=strtolower($value);
-		}
-	}
+    public function setSegundoNombreAttibute($value)
+    {
+        if (!empty($value)) {
+            $this->attributes['segundo_nombre'] = strtolower($value);
+        }
+    }
 
 
-	public function setPrimerApellidoAttibute($value){
-		if (!empty($value)) {
-			$this->attributes['primer_apellido']=strtolower($value);
-		}
-	}
+    public function setPrimerApellidoAttibute($value)
+    {
+        if (!empty($value)) {
+            $this->attributes['primer_apellido'] = strtolower($value);
+        }
+    }
 
 
-	public function setSegundoApellidoAttibute($value){
-		if (!empty($value)) {
-			$this->attributes['segundo_apellido']=strtolower($value);
-		}
-	}
+    public function setSegundoApellidoAttibute($value)
+    {
+        if (!empty($value)) {
+            $this->attributes['segundo_apellido'] = strtolower($value);
+        }
+    }
 
-	
+    public function get_nombre_completo()
+    {
+        return ucwords($this->primer_nombre . " " . $this->primer_apellido);
+    }
+
+
 }
 
 ?>
