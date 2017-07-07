67d37a20d7fb7d3c31ba30499a6540ec27e35ecf
danztty <danztty@gmail.com>
2017-07-07 11:23:21 -0400

    [MOD] Modificada la vista consultas

--- app/views/consultas/ver_consultas.blade.php
+++ app/views/consultas/ver_consultas.blade.php
@@ -1,8 +1,47 @@
-<!--Bloque 1 -> Tabla Principal-->
-<div class="ui two column doubling stackable grid container">
-   <div class="ui container centered grid">
-      <div class="column">
-         <table class="ui selectable celled table capitalize" datatable="" dt-options="opciones_tabla_consultas" dt-columns="columnas_tabla_consultas" dt-instance='tabla_consultas' width="100%"></table>
-      </div>
-   </div>
+<div class="ui centered grid">
+    <div class="six wide tablet twelve wide computer column">
+        <div class="ui form">
+
+            <div ng-if="mostrar_mensaje">
+                <div class="ui icon <% mensaje_validacion.color %> message">
+                    <i class="<% mensaje_validacion.icono %> icon"></i>
+                    <div class="content">
+                        <div class="header"><% mensaje_validacion.titulo %></div>
+                        <ul class="list">
+                            <li ng-repeat=" mensaje in mensaje_validacion.mensajes track by $index"><% mensaje |
+                                capitalize %>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <br>
+            </div>
+
+            <form class="ui form" id="formulario_crear_tipo_usuario">
+                <h3 class="ui centered dividing header">Generar Consulta</h3>
+
+                <div class="field">
+                    <div class="six wide field">
+                        <div class="field">
+                            <label>Nombre</label>
+                            <input type="text" name="nombre" placeholder="Nombre consulta" ng-model="DatosForm.nombre">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="field">
+                    <div class="nine wide field ui form">
+                        <div class="field">
+                            <label>Consulta</label>
+                            <textarea name="descripcion" placeholder="Aca va la consulta en SQL"
+                                      ng-model="DatosForm.descripcion" rows="4"></textarea>
+                        </div>
+                    </div>
+                </div>
+                <div class="ui big right floated submit button green" ng-click="generar_consulta()"
+                     id="btn-registrar"> Procesar
+                </div>
+            </form>
+        </div>
+    </div>
 </div>
